3aa1e7c102673fc778e73a50ae1ba498c4d7ccc6
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2003-05-14 13:51:35 +0000

    restructuring

--- frost-wot/source/frost/gui/BoardSettingsFrame.java
+++ frost-wot/source/frost/gui/BoardSettingsFrame.java
@@ -31,7 +31,8 @@ import java.util.*;
 import frost.*;
 import frost.FcpTools.*;
 
-public class BoardSettingsFrame extends JDialog {
+public class BoardSettingsFrame extends JDialog
+{
 
     //------------------------------------------------------------------------
     // Class Vars
@@ -66,87 +67,97 @@ public class BoardSettingsFrame extends JDialog {
     JTextField publicKeyTextField = new JTextField(32);
 
     private void Init() throws Exception {
-    //------------------------------------------------------------------------
-    // Configure objects
-    //------------------------------------------------------------------------
-    this.setTitle(LangRes.getString("Board settings for ") + board);
-    this.setResizable(false);
-    this.setSize(new Dimension(680, 480));
+        //------------------------------------------------------------------------
+        // Configure objects
+        //------------------------------------------------------------------------
+        this.setTitle(LangRes.getString("Board settings for ") + board);
+        this.setResizable(false);
+        this.setSize(new Dimension(680, 480));
 
-    //------------------------------------------------------------------------
-    // Actionlistener
-    //------------------------------------------------------------------------
+        //------------------------------------------------------------------------
+        // Actionlistener
+        //------------------------------------------------------------------------
 
-    // Public board radio button
-    publicBoardRadioButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            radioButton_actionPerformed(e);
-        }
-        });
+        // Public board radio button
+        publicBoardRadioButton.addActionListener(new java.awt.event.ActionListener()
+                                                 {
+                                                     public void actionPerformed(ActionEvent e)
+                                                     {
+                                                         radioButton_actionPerformed(e);
+                                                     }
+                                                 });
 
-    // Private board radio button
-    secureBoardRadioButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            radioButton_actionPerformed(e);
-        }
-        });
+        // Private board radio button
+        secureBoardRadioButton.addActionListener(new java.awt.event.ActionListener()
+                                                 {
+                                                     public void actionPerformed(ActionEvent e)
+                                                     {
+                                                         radioButton_actionPerformed(e);
+                                                     }
+                                                 });
 
-    // generate key
-    generateKeyButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            generateKeyButton_actionPerformed(e);
-        }
-        });
+        // generate key
+        generateKeyButton.addActionListener(new java.awt.event.ActionListener()
+                                            {
+                                                public void actionPerformed(ActionEvent e)
+                                                {
+                                                    generateKeyButton_actionPerformed(e);
+                                                }
+                                            });
 
-    // Ok
-    okButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            okButton_actionPerformed(e);
-        }
-        });
+        // Ok
+        okButton.addActionListener(new java.awt.event.ActionListener()
+                                   {
+                                       public void actionPerformed(ActionEvent e)
+                                       {
+                                           okButton_actionPerformed(e);
+                                       }
+                                   });
 
-    // Cancel
-    cancelButton.addActionListener(new java.awt.event.ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            cancelButton_actionPerformed(e);
-        }
-        });
+        // Cancel
+        cancelButton.addActionListener(new java.awt.event.ActionListener()
+                                       {
+                                           public void actionPerformed(ActionEvent e)
+                                           {
+                                               cancelButton_actionPerformed(e);
+                                           }
+                                       });
 
-    //------------------------------------------------------------------------
-    // Append objects
-    //------------------------------------------------------------------------
+        //------------------------------------------------------------------------
+        // Append objects
+        //------------------------------------------------------------------------
 
-    this.getContentPane().add(mainPanel, null); // add Main panel
+        this.getContentPane().add(mainPanel, null); // add Main panel
 
-    group.add(publicBoardRadioButton);
-    group.add(secureBoardRadioButton);
-    radioButtonPanel.add(publicBoardRadioButton, BorderLayout.NORTH);
-    radioButtonPanel.add(secureBoardRadioButton, BorderLayout.SOUTH);
+        group.add(publicBoardRadioButton);
+        group.add(secureBoardRadioButton);
+        radioButtonPanel.add(publicBoardRadioButton, BorderLayout.NORTH);
+        radioButtonPanel.add(secureBoardRadioButton, BorderLayout.SOUTH);
 
-    // key panel
-    privateKeyPanel.add(new Label(LangRes.getString("Private key :")), BorderLayout.WEST);
-    privateKeyPanel.add(privateKeyTextField, BorderLayout.EAST);
-    publicKeyPanel.add(new Label(LangRes.getString("Public key :")), BorderLayout.WEST);
-    publicKeyPanel.add(publicKeyTextField, BorderLayout.EAST);
-    privateKeyTextField.setEnabled(false);
-    publicKeyTextField.setEnabled(false);
-    generateKeyButton.setEnabled(false);
+        // key panel
+        privateKeyPanel.add(new Label(LangRes.getString("Private key :")), BorderLayout.WEST);
+        privateKeyPanel.add(privateKeyTextField, BorderLayout.EAST);
+        publicKeyPanel.add(new Label(LangRes.getString("Public key :")), BorderLayout.WEST);
+        publicKeyPanel.add(publicKeyTextField, BorderLayout.EAST);
+        privateKeyTextField.setEnabled(false);
+        publicKeyTextField.setEnabled(false);
+        generateKeyButton.setEnabled(false);
 
-    keyPanel.add(privateKeyPanel);
-    keyPanel.add(publicKeyPanel);
-    keyPanel.add(generateKeyButton);
+        keyPanel.add(privateKeyPanel);
+        keyPanel.add(publicKeyPanel);
+        keyPanel.add(generateKeyButton);
 
-    // Main
-    mainPanel.add(radioButtonPanel, BorderLayout.NORTH);
-    mainPanel.add(keyPanel, BorderLayout.CENTER);
-    mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        // Main
+        mainPanel.add(radioButtonPanel, BorderLayout.NORTH);
+        mainPanel.add(keyPanel, BorderLayout.CENTER);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
-    // OK / Cancel buttons
-    buttonPanel.add(okButton);
-    buttonPanel.add(cancelButton);
+        // OK / Cancel buttons
+        buttonPanel.add(okButton);
+        buttonPanel.add(cancelButton);
 
-    publicBoardRadioButton.setSelected(true);
-    loadKeypair();
+        publicBoardRadioButton.setSelected(true);
+        loadKeypair();
     }
 
     //------------------------------------------------------------------------
@@ -154,148 +165,172 @@ public class BoardSettingsFrame extends JDialog {
     //------------------------------------------------------------------------
 
     /**radioButton Action Listener (OK)*/
-    private void radioButton_actionPerformed(ActionEvent e) {
-    if (publicBoardRadioButton.isSelected()) {
-        privateKeyTextField.setEnabled(false);
-        publicKeyTextField.setEnabled(false);
-        generateKeyButton.setEnabled(false);
-    }
-    else {
-        privateKeyTextField.setEnabled(true);
-        publicKeyTextField.setEnabled(true);
-        generateKeyButton.setEnabled(true);
-    }
+    private void radioButton_actionPerformed(ActionEvent e)
+    {
+        if( publicBoardRadioButton.isSelected() )
+        {
+            privateKeyTextField.setEnabled(false);
+            publicKeyTextField.setEnabled(false);
+            generateKeyButton.setEnabled(false);
+        }
+        else
+        {
+            privateKeyTextField.setEnabled(true);
+            publicKeyTextField.setEnabled(true);
+            generateKeyButton.setEnabled(true);
+        }
     }
 
     /**generateKeyButton Action Listener (OK)*/
-    private void generateKeyButton_actionPerformed(ActionEvent e) {
-    try {
-        FcpConnection connection = new FcpConnection(frame1.frostSettings.getValue("nodeAddress"), frame1.frostSettings.getValue("nodePort"));
-        try {
-        String[] keyPair = connection.getKeyPair();
-        privateKeyTextField.setText(keyPair[0]);
-        publicKeyTextField.setText(keyPair[1]);
+    private void generateKeyButton_actionPerformed(ActionEvent e)
+    {
+        try
+        {
+            FcpConnection connection = new FcpConnection(frame1.frostSettings.getValue("nodeAddress"), frame1.frostSettings.getValue("nodePort"));
+            try
+            {
+                String[] keyPair = connection.getKeyPair();
+                privateKeyTextField.setText(keyPair[0]);
+                publicKeyTextField.setText(keyPair[1]);
+            }
+            catch( IOException ex )
+            {
+                frame1.displayWarning(ex.toString());
+            }
         }
-        catch (IOException ex) {
-        frame1.displayWarning(ex.toString());
+        catch( FcpToolsException ex )
+        {
+            System.out.println("FcpToolsException " + ex);
         }
-    }
-    catch (FcpToolsException ex) {
-        System.out.println("FcpToolsException " + ex);
-    }
-    catch (IOException ex) {
-        frame1.displayWarning(ex.toString());
-    }
+        catch( IOException ex )
+        {
+            frame1.displayWarning(ex.toString());
+        }
     }
 
     /**okButton Action Listener (OK)*/
-    private void okButton_actionPerformed(ActionEvent e) {
-    ok();
+    private void okButton_actionPerformed(ActionEvent e)
+    {
+        ok();
     }
 
     /**cancelButton Action Listener (Cancel)*/
-    private void cancelButton_actionPerformed(ActionEvent e) {
-    cancel();
+    private void cancelButton_actionPerformed(ActionEvent e)
+    {
+        cancel();
     }
 
     //------------------------------------------------------------------------
 
     /**Return exitState*/
-    public boolean getExitState() {
-    return exitState;
+    public boolean getExitState()
+    {
+        return exitState;
     }
 
     /**Return exitState*/
-    public String getReturnValue() {
-    return returnValue;
+    public String getReturnValue()
+    {
+        return returnValue;
     }
 
     //------------------------------------------------------------------------
 
     /**Close window and save settings*/
-    private void ok() {
-    exitState = true;
-    returnValue = "publicBoard";
-    String privateKey = privateKeyTextField.getText();
-    String publicKey = publicKeyTextField.getText();
-    File keyFile = new File(frame1.keypool + board + ".key");
+    private void ok()
+    {
+        exitState = true;
+        returnValue = "publicBoard";
+        String privateKey = privateKeyTextField.getText();
+        String publicKey = publicKeyTextField.getText();
+        File keyFile = new File(frame1.keypool + board + ".key");
 
-    if (secureBoardRadioButton.isSelected()) {
+        if( secureBoardRadioButton.isSelected() )
+        {
 
-        if (publicKey.startsWith("SSK@") && !privateKey.startsWith("SSK@"))
-        returnValue = "readAccess";
+            if( publicKey.startsWith("SSK@") && !privateKey.startsWith("SSK@") )
+                returnValue = "readAccess";
 
-        if (publicKey.startsWith("SSK@") && privateKey.startsWith("SSK@"))
-        returnValue = "writeAccess";
+            if( publicKey.startsWith("SSK@") && privateKey.startsWith("SSK@") )
+                returnValue = "writeAccess";
 
-    }
-    String lineSeparator = System.getProperty("line.separator");
-    String text = "privateKey=" + privateKey + lineSeparator;
-    text += "publicKey=" + publicKey + lineSeparator;
-    text += "state=" + returnValue + lineSeparator;
-    FileAccess.writeFile(text, frame1.keypool + board + ".key");
+        }
+        String lineSeparator = System.getProperty("line.separator");
+        String text = "privateKey=" + privateKey + lineSeparator;
+        text += "publicKey=" + publicKey + lineSeparator;
+        text += "state=" + returnValue + lineSeparator;
+        FileAccess.writeFile(text, frame1.keypool + board + ".key");
 
-    dispose();
+        dispose();
     }
 
     /**Close window and do not save settings*/
-    private void cancel() {
-    exitState = false;
-    dispose();
+    private void cancel()
+    {
+        exitState = false;
+        dispose();
     }
 
     /** Loads keypair from file */
-    private void loadKeypair() {
-    File keyFile = new File(frame1.keypool + board + ".key");
-    privateKeyTextField.setText(LangRes.getString("Not available"));
-    publicKeyTextField.setText(LangRes.getString("Not available"));
+    private void loadKeypair()
+    {
+        File keyFile = new File(frame1.keypool + board + ".key");
+        privateKeyTextField.setText(LangRes.getString("Not available"));
+        publicKeyTextField.setText(LangRes.getString("Not available"));
 
-    if (keyFile.isFile() && keyFile.length() > 0) {
-        String privateKey = SettingsFun.getValue(keyFile, "privateKey");
-        String publicKey = SettingsFun.getValue(keyFile, "publicKey");
-        String state = SettingsFun.getValue(keyFile, "state");
-        if (!privateKey.equals(""))
-        privateKeyTextField.setText(privateKey);
-        if (!publicKey.equals(""))
-        publicKeyTextField.setText(publicKey);
+        if( keyFile.isFile() && keyFile.length() > 0 )
+        {
+            String privateKey = SettingsFun.getValue(keyFile, "privateKey");
+            String publicKey = SettingsFun.getValue(keyFile, "publicKey");
+            String state = SettingsFun.getValue(keyFile, "state");
+            if( !privateKey.equals("") )
+                privateKeyTextField.setText(privateKey);
+            if( !publicKey.equals("") )
+                publicKeyTextField.setText(publicKey);
 
-        if (state.equals("writeAccess") || state.equals("readAccess")) {
-        privateKeyTextField.setEnabled(true);
-        publicKeyTextField.setEnabled(true);
-        generateKeyButton.setEnabled(true);
-        secureBoardRadioButton.setSelected(true);
+            if( state.equals("writeAccess") || state.equals("readAccess") )
+            {
+                privateKeyTextField.setEnabled(true);
+                publicKeyTextField.setEnabled(true);
+                generateKeyButton.setEnabled(true);
+                secureBoardRadioButton.setSelected(true);
+            }
+            else
+            {
+                privateKeyTextField.setEnabled(false);
+                publicKeyTextField.setEnabled(false);
+                generateKeyButton.setEnabled(false);
+                publicBoardRadioButton.setSelected(true);
+            }
         }
-        else {
-        privateKeyTextField.setEnabled(false);
-        publicKeyTextField.setEnabled(false);
-        generateKeyButton.setEnabled(false);
-        publicBoardRadioButton.setSelected(true);
-        }
-    }
     }
 
-    protected void processWindowEvent(WindowEvent e) {
-    if (e.getID() == WindowEvent.WINDOW_CLOSING) {
-        dispose();
-    }
-    super.processWindowEvent(e);
+    protected void processWindowEvent(WindowEvent e)
+    {
+        if( e.getID() == WindowEvent.WINDOW_CLOSING )
+        {
+            dispose();
+        }
+        super.processWindowEvent(e);
     }
 
     /**Constructor*/
-    public BoardSettingsFrame(Frame parent, String board) {
-    super(parent);
-    this.board = board.toLowerCase();
+    public BoardSettingsFrame(Frame parent, String board)
+    {
+        super(parent);
+        this.board = board.toLowerCase();
 
-    enableEvents(AWTEvent.WINDOW_EVENT_MASK);
-    try {
-        Init();
-    }
-    catch(Exception e) {
-        e.printStackTrace();
-    }
-    pack();
-    setLocationRelativeTo(parent);
+        enableEvents(AWTEvent.WINDOW_EVENT_MASK);
+        try
+        {
+            Init();
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
+        pack();
+        setLocationRelativeTo(parent);
     }
 }
-
 
