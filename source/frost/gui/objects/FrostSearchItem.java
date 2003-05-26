5030db56f74c2cd03ab72ec3250d9b01f923b906
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2003-05-26 12:52:23 +0000

    enhanced FrostSearchItem

--- source/frost/gui/objects/FrostSearchItem.java
+++ source/frost/gui/objects/FrostSearchItem.java
@@ -2,5 +2,13 @@ package frost.gui.objects;
 
 public interface FrostSearchItem
 {
+    public String getFilename();
 
+    public Long getSize();
+
+    public String getDate();
+
+    public String getKey();
+
+    public FrostBoardObject getBoard();
 }
