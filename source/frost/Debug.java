65ca11bdb5510600f06c628450e66553885bc576
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2003-06-04 12:04:38 +0000

    added Debug.java

---
+++ source/frost/Debug.java
@@ -0,0 +1,36 @@
+package frost;
+
+import java.util.*;
+
+// CLASS IS NOT USED YET ... BUT IT WILL!
+
+public class Debug
+{
+    private static boolean debug = true;
+
+    /**
+     * Sets the debug mode.
+     */
+    public static void setDebug(boolean v)
+    {
+        debug = v;
+    }
+
+    /**
+     * Returns true if debug mode is enabled.
+     */
+    public static boolean debug()
+    {
+        return debug;
+    }
+
+    /**
+     * Returns true if debug mode is enabled AND if localDebug is also true.
+     * Useable if you have a class variable MYDEBUG and check if debug should be written by:
+     *  if(Debug.debug(MYDEBUG)) writeMyDebugOutput()
+     */
+    public static boolean debug( boolean localDebug )
+    {
+        return debug && localDebug;
+    }
+}
