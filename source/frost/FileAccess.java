1a0721e727fefdabb304f24df72551aac4af2354
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2003-06-13 11:24:10 +0000

    fixes

--- source/frost/FileAccess.java
+++ source/frost/FileAccess.java
@@ -489,4 +489,27 @@ public class FileAccess
 
         return filenames;
     }
+
+    /**
+     * Deletes the given directory and ALL FILES/DIRS IN IT !!!
+     * USE CAREFUL !!!
+     */
+    public static boolean deleteDir(File dir)
+    {
+        if (dir.isDirectory())
+        {
+            String[] children = dir.list();
+            for (int i=0; i<children.length; i++)
+            {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success)
+                {
+                    return false;
+                }
+            }
+        }
+        // The directory is now empty so delete it
+        return dir.delete();
+    }
+
 }
