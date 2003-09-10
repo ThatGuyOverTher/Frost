2576bcc29d688e3a5416497d87046fbcbea0e250
zlatinb <zlatinb@unknown>
2003-09-10 02:40:12 +0000

    trying things made with eclipse :)

---
+++ frost-wot/source/frost/stats/Stat.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Sep 9, 2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package frost.stats;
+
+/**
+ * @author root
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+interface Stat {
+	public Object getTarget();
+	public String getName();
+
+}
