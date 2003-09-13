e160755cd00b6195c864b73fddaf5563f27d613e
zlatinb <zlatinb@unknown>
2003-09-13 22:35:57 +0000

    more refactoring

---
+++ frost-wot/source/frost/threads/support/ResendFailedMessagesThread.java
@@ -0,0 +1,78 @@
+/*
+ * Created on Sep 13, 2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package frost.threads.support;
+
+import java.awt.Frame;
+import java.io.File;
+import java.util.ArrayList;
+
+import frost.Core;
+import frost.FileAccess;
+import frost.VerifyableMessageObject;
+import frost.frame1;
+import frost.gui.objects.FrostBoardObject;
+
+
+public class ResendFailedMessagesThread extends Thread
+{
+	private final Core core;
+    Frame frameToLock;
+    public ResendFailedMessagesThread(Core core, Frame frameToLock)
+    {
+        this.frameToLock = frameToLock;
+		this.core = core;
+    }
+    public void run()
+    {
+        // give gui a chance to appear ... then start searching for unsent messages
+        try { Thread.sleep(10000); } // wait 10 seconds
+        catch(InterruptedException ex) { ; }
+        if( isInterrupted() )
+            return;
+
+        System.out.println("Starting search for unsent messages ...");
+
+        ArrayList entries = FileAccess.getAllEntries(new File(Core.frostSettings.getValue("unsent.dir")), ".txt");
+
+        for( int i = 0; i < entries.size(); i++ )
+        {
+            File unsentMsgFile = (File)entries.get(i);
+            if( unsentMsgFile.getName().startsWith("unsent") )
+            {
+                // Resend message
+                VerifyableMessageObject mo = new VerifyableMessageObject(unsentMsgFile);
+                if( mo.isValid() )
+                {
+                    FrostBoardObject board = frame1.getInstance().getTofTree().getBoardByName( mo.getBoard() );
+                    if( board == null )
+                    {
+                        System.out.println("Can't resend Message '"+mo.getSubject()+"', the target board '"+mo.getBoard()+
+                                           "' was not found in your boardlist.");
+                        // TODO: maybe delete msg? or it will always be retried to send
+                        continue;
+                    }
+                    // message will be resigned before send, actual date/time will be used
+                    // no more faking here :)
+                    frame1.getInstance().getRunningBoardUpdateThreads().startMessageUpload(
+                        board,
+                        mo.getFrom(),
+                        mo.getSubject(),
+                        mo.getContent(),
+                        "",
+                        Core.frostSettings,
+                        frameToLock,
+                        null);
+                    System.out.println("Message '" + mo.getSubject() + "' will be resent to board '"+board.toString()+"'.");
+                }
+                // check if upload was successful before deleting the file -
+                // is not needed, the upload thread creates new unsent file
+                unsentMsgFile.delete();
+            }
+        }
+        System.out.println("Finished search for unsent messages ...");
+    }
+}
---
+++ frost-wot/source/frost/threads/support/Truster.java
@@ -0,0 +1,152 @@
+/*
+ * Created on Sep 13, 2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package frost.threads.support;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.SwingUtilities;
+
+import frost.Core;
+import frost.FileAccess;
+import frost.TOF;
+import frost.VerifyableMessageObject;
+import frost.frame1;
+import frost.gui.objects.FrostMessageObject;
+import frost.identities.Identity;
+
+
+/**
+ * Thread is invoked if the Trust or NotTrust button is clicked.
+ */
+public class Truster extends Thread
+{
+	private final Core core;
+    private Boolean trust;
+    private Identity newIdentity;
+    private VerifyableMessageObject currentMsg;
+
+    public Truster(Core core, Boolean what, VerifyableMessageObject msg)
+    {
+        trust=what;
+		this.core = core;
+        currentMsg = msg;
+    }
+
+    public void run()
+    {
+        String from = currentMsg.getFrom();
+        String newState;
+
+        if( trust == null )  newState = "CHECK";
+        else if( trust.booleanValue() == true ) newState = "GOOD";
+        else newState = "BAD";
+
+        System.out.println("Truster: Setting '"+
+                           from+
+                           "' to '"+
+                           newState+
+                           "'.");
+
+        if( trust == null )
+        {
+            // set enemy/friend to CHECK
+            newIdentity = Core.friends.Get(from);
+            if( newIdentity==null )
+                newIdentity=Core.enemies.Get(from);
+
+            if( newIdentity == null ) // not found -> paranoia
+            {
+                newIdentity = new Identity(currentMsg.getFrom(), currentMsg.getKeyAddress());
+            }
+            else
+            {
+                Core.friends.remove( from );
+                Core.enemies.remove( from );
+            }
+        }
+        else if( Core.friends.containsKey(from) && trust.booleanValue() == false )
+        {
+            // set friend to bad
+            newIdentity = Core.friends.Get(from);
+            Core.friends.remove( from );
+            Core.enemies.Add( newIdentity );
+        }
+        else if( Core.enemies.containsKey(from) && trust.booleanValue() == true )
+        {
+            // set enemy to good
+            newIdentity = Core.enemies.Get(from);
+            Core.enemies.remove( newIdentity );
+            Core.friends.Add( newIdentity );
+        }
+        else
+        {
+            // new new enemy/friend
+            newIdentity = new Identity(currentMsg.getFrom(), currentMsg.getKeyAddress());
+            if( trust.booleanValue() )
+                Core.friends.Add(newIdentity);
+            else
+                Core.enemies.Add(newIdentity);
+        }
+
+        if( newIdentity == null || Identity.NA.equals( newIdentity.getKey() ) )
+        {
+            System.out.println("Truster - ERROR: could not get public key for '"+currentMsg.getFrom()+"'");
+            System.out.println("Truster: Will stop to set message states!!!");
+            return;
+        }
+
+        // get all .txt files in keypool
+        ArrayList entries = FileAccess.getAllEntries( new File(frame1.frostSettings.getValue("keypool.dir")),
+                                                   ".txt");
+        System.out.println("Truster: Starting to update messages:");
+
+        for( int ii=0; ii<entries.size(); ii++ )
+        {
+            File msgFile = (File)entries.get(ii);
+            FrostMessageObject tempMsg = new FrostMessageObject( msgFile );
+            if( tempMsg.getFrom().equals(currentMsg.getFrom()) &&
+                (
+                  tempMsg.getStatus().trim().equals(VerifyableMessageObject.PENDING) ||
+                  tempMsg.getStatus().trim().equals(VerifyableMessageObject.VERIFIED) ||
+                  tempMsg.getStatus().trim().equals(VerifyableMessageObject.FAILED)
+                )
+              )
+            {
+                // check if message is correctly signed
+                if( newIdentity.getKeyAddress().equals( tempMsg.getKeyAddress() ) &&
+                    Core.getCrypto().verify(tempMsg.getContent(), newIdentity.getKey()) )
+                {
+                    // set new state of message
+                    if( trust == null )
+                        tempMsg.setStatus(VerifyableMessageObject.PENDING);
+                    else if( trust.booleanValue() )
+                        tempMsg.setStatus(VerifyableMessageObject.VERIFIED);
+                    else
+                        tempMsg.setStatus(VerifyableMessageObject.FAILED);
+
+                    System.out.print("."); // progress
+                }
+                else
+                {
+                    System.out.println("\n!Truster: Could not verify message, maybe the message is faked!" +
+                                       " Message state set to N/A for '"+msgFile.getPath()+"'.");
+                    tempMsg.setStatus(VerifyableMessageObject.NA);
+                }
+            }
+        }
+        // finally step through all board files, count new messages and delete new messages from enemies
+        TOF.initialSearchNewMessages();
+
+        SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    frame1.getInstance().tofTree_actionPerformed(null);
+                } });
+        System.out.println("\nTruster: Finished to update messages, set '"+currentMsg.getFrom()+"' to '"+
+                           newState+"'");
+    }
+}
