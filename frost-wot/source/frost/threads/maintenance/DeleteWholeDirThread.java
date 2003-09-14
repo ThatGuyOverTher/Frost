40a84a3e6d8d8c2ff26b10b43dbaaef3ce6d617d
zlatinb <zlatinb@unknown>
2003-09-14 02:15:55 +0000

    eclipse is so much fun

---
+++ frost-wot/source/frost/threads/maintenance/DeleteWholeDirThread.java
@@ -0,0 +1,28 @@
+/*
+ * Created on Sep 13, 2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package frost.threads.maintenance;
+
+import java.io.File;
+
+import frost.Core;
+import frost.FileAccess;
+
+
+public class DeleteWholeDirThread extends Thread
+{
+	private final Core core;
+    String delDir;
+    public DeleteWholeDirThread(Core core, String dirToDelete)
+    {
+        delDir = dirToDelete;
+		this.core = core;
+    }
+    public void run()
+    {
+        FileAccess.deleteDir( new File(delDir) );
+    }
+}
