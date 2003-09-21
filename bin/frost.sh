2189c5d57d6f4cc6c54fcfaec9a5269719e43f07
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2003-09-21 12:58:14 +0000

    fixes

--- bin/frost.sh
+++ bin/frost.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-
 JARDIR=.
-
-CLASSPATH="$JARDIR/frost.jar:$JARDIR/lib/fecImpl.jar:$JARDIR/lib/genChkImpl.jar:$JARDIR/lib/xercesImpl.jar:$JARDIR/lib/xml-apis.jar:$JARDIR/lib/xmlParserAPIs.jar:$CLASSPATH"
+CLASSPATH="$JARDIR/frost.jar:$JARDIR/lib/fecImpl.jar:$JARDIR/lib/genChkImpl.jar:$JARDIR/lib/xercesImpl.jar:$JARDIR/lib/xml-apis.jar:$CLASSPATH"
 export CLASSPATH
-
 java frost.frost "$@"
