0a79597c73c2abcbd167be7d446a0aa5cde09ce7
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2003-10-17 10:47:08 +0000

    Added skinlfFix.jar

--- frost-wot/bin/frost_noDosBox.bat
+++ frost-wot/bin/frost_noDosBox.bat
@@ -1,1 +1,1 @@
-start javaw -cp lib/fecImpl.jar;lib/genChkImpl.jar;lib/xercesImpl.jar;lib/xml-apis.jar;lib/BCastle.jar;lib/skinlf.jar;.;frost.jar frost.frost %1 %2 %3 %4 %5 %6 %7
+start javaw -cp lib/fecImpl.jar;lib/genChkImpl.jar;lib/xercesImpl.jar;lib/xml-apis.jar;lib/BCastle.jar;lib/skinlfFix.jar;lib/skinlf.jar;.;frost.jar frost.frost %1 %2 %3 %4 %5 %6 %7
