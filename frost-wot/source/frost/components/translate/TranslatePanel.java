ad384387ac20561d5e3ed272446e01b346c1cd98
jantho <jantho@unknown>
2003-11-06 12:47:20 +0000

    added instructions to translate tool

--- frost-wot/source/frost/components/translate/TranslatePanel.java
+++ frost-wot/source/frost/components/translate/TranslatePanel.java
@@ -37,147 +37,167 @@ import javax.swing.*;
  * @author Jantho
  */
 public class TranslatePanel extends JPanel {
-    
-    static java.util.ResourceBundle LangRes = java.util.ResourceBundle.getBundle("res.LangRes");
 
-    JFrame parent;
-    
-    //
-    // GUI objects
-    //
-    JPanel contentPanel;
+	static java.util.ResourceBundle LangRes =
+		java.util.ResourceBundle.getBundle("res.LangRes");
 
-    //Table for original text and translation
-    TranslateTableModel tableModel = new TranslateTableModel();
-    JTable table = new JTable(tableModel);
-    JScrollPane scrollPane = new JScrollPane(table);
+	JFrame parent;
 
-    //ToolBar
-    JToolBar toolBar = new JToolBar();
-    JComboBox comboBox = new JComboBox();
-    JButton saveButton = new JButton("Save translation");
-    JButton loadButton = new JButton("Load translation from file");
+	//
+	// GUI objects
+	//
+	JPanel contentPanel;
 
-    /**Constructor*/
-    public TranslatePanel(JFrame parent) {
-	this.parent = parent;
-	init();
-    }
+	//Table for original text and translation
+	TranslateTableModel tableModel = new TranslateTableModel();
+	JTable table = new JTable(tableModel);
+	JScrollPane scrollPane = new JScrollPane(table);
 
-    /**Initialize the panel*/
-    private void init() {
-	//assemble gui objects
-	generateComboBox();
-	comboBox.setMaximumRowCount(25);
+	//ToolBar
+	JToolBar toolBar = new JToolBar();
+	JComboBox comboBox = new JComboBox();
+	JButton saveButton = new JButton("Save translation");
+	JButton loadButton = new JButton("Load translation from file");
 
-	toolBar.add(comboBox);
-	toolBar.add(saveButton);
-	toolBar.add(loadButton);
-
-	contentPanel = this;
-	contentPanel.setLayout(new BorderLayout());		
-	contentPanel.add(toolBar, BorderLayout.NORTH);
-	contentPanel.add(scrollPane, BorderLayout.CENTER);
-
-	generateButtonListeners();
-	generateComboBoxListeners();
-
-	configureTable("en");
-    }
-    
-    /**
-     * Clears the table, get's the strings for the
-     * requested language and inserts them into
-     * the tableModel
-     * @param language Language strings as defined in the locale object
-     */
-    private void configureTable(String language) {
-	java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("res.LangRes", 
-									     new Locale(getSelectedLanguageCode()));
-	
-	tableModel.setRowCount(0);
-	for (Enumeration e = bundle.getKeys(); e.hasMoreElements(); ) {
-	    String originalText = (String)e.nextElement();
-	    String row[] = {originalText, bundle.getString(originalText)};
-	    tableModel.addRow(row);
+	/**Constructor*/
+	public TranslatePanel(JFrame parent) {
+		this.parent = parent;
+		init();
 	}
-    }
 
-    /**
-     * Enters some default entries into the
-     * comboBox.
-     */
-    private void generateComboBox() {
-	Locale actualLocale = LangRes.getLocale();
-	Locale availableLocales[] = actualLocale.getAvailableLocales();
-	for (int i = 0; i < availableLocales.length; i++) {
-	    comboBox.addItem(availableLocales[i].getDisplayName() + " (" +  
-			     availableLocales[i].getLanguage() + ")");
+	/**Initialize the panel*/
+	private void init() {
+		//assemble gui objects
+		generateComboBox();
+		comboBox.setMaximumRowCount(25);
+
+		toolBar.add(comboBox);
+		toolBar.add(saveButton);
+		toolBar.add(loadButton);
+
+		contentPanel = this;
+		contentPanel.setLayout(new BorderLayout());
+		contentPanel.add(toolBar, BorderLayout.NORTH);
+		contentPanel.add(scrollPane, BorderLayout.CENTER);
+
+		generateButtonListeners();
+		generateComboBoxListeners();
+
+		//Select english language by default
+		configureTable("en");
+		for (int i = 0; i < comboBox.getItemCount(); i++) {
+			if (((String) comboBox.getItemAt(i)).equals("English (en)")) {
+				comboBox.setSelectedIndex(i);
+				break;
+			}
+		}
+
+		// Display some short instructions in a message box
+		JOptionPane.showMessageDialog(
+			parent,
+			"1. Select the language you want to translate.\n2. Translate each row of the table.\n3. Save your translation\n4. Send us the saved 'LangRes'  file in your Frost directory\n   or upload it with frost.\n\nThank you!",
+			"Instructions",
+			JOptionPane.PLAIN_MESSAGE);
 	}
-    }
 
-    /**
-     * Adds action listeners for the comboBox
-     */
-    private void generateComboBoxListeners() {
-	comboBox.addItemListener(new java.awt.event.ItemListener() {
-		public void itemStateChanged(java.awt.event.ItemEvent e) {
-		    configureTable((String)comboBox.getSelectedItem());
+	/**
+	 * Clears the table, get's the strings for the
+	 * requested language and inserts them into
+	 * the tableModel
+	 * @param language Language strings as defined in the locale object
+	 */
+	private void configureTable(String language) {
+		java.util.ResourceBundle bundle =
+			java.util.ResourceBundle.getBundle(
+				"res.LangRes",
+				new Locale(getSelectedLanguageCode()));
+
+		tableModel.setRowCount(0);
+		for (Enumeration e = bundle.getKeys(); e.hasMoreElements();) {
+			String originalText = (String) e.nextElement();
+			String row[] = { originalText, bundle.getString(originalText)};
+			tableModel.addRow(row);
 		}
-	    });
-    }
+	}
 
-    private String getSelectedLanguageCode() {
-	String selectedElement = (String)comboBox.getSelectedItem();
-	String languageCode = selectedElement.substring(selectedElement.length() - 3, 
-							selectedElement.length() - 1);
-	return languageCode;
-    }
-
-    /**
-     * Generates button listeners
-     */
-    private void generateButtonListeners() {
-	saveButton.addActionListener(new java.awt.event.ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    String languageCode = getSelectedLanguageCode();
-		    String content = LanguageFile.generateFile(tableModel, languageCode);
-		    if (languageCode.equals("en")) {
-			WriteAccess.writeFile(content, 
-					      new File("LangRes.java"));
-		    }
-		    else {
-			WriteAccess.writeFile(content, 
-					      new File("LangRes_" + languageCode + ".java"));
-		    }
+	/**
+	 * Enters some default entries into the
+	 * comboBox.
+	 */
+	private void generateComboBox() {
+		Locale actualLocale = LangRes.getLocale();
+		Locale availableLocales[] = actualLocale.getAvailableLocales();
+		for (int i = 0; i < availableLocales.length; i++) {
+			comboBox.addItem(
+				availableLocales[i].getDisplayName()
+					+ " ("
+					+ availableLocales[i].getLanguage()
+					+ ")");
 		}
-	    });
-	loadButton.addActionListener(new java.awt.event.ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    loadLanguageFile();
-		}
-	    });
-    }
-    
-    private void loadLanguageFile() {
-        final JFileChooser fc = new JFileChooser(new File("."));
-        fc.setDialogTitle("Select " + getSelectedLanguageCode() + " language file");
-        fc.setFileHidingEnabled(true);
-        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        fc.setMultiSelectionEnabled(false);
+	}
 
-        int returnVal = fc.showOpenDialog(this);
-        if( returnVal == JFileChooser.APPROVE_OPTION )
-        {
-            File file = fc.getSelectedFile();
-            if( file != null )
-            {
-                if( !file.isDirectory() )
-                {
-		    LanguageFile.readLanguageFile(tableModel, file);
-                }
-            }
-        }
-    }
+	/**
+	 * Adds action listeners for the comboBox
+	 */
+	private void generateComboBoxListeners() {
+		comboBox.addItemListener(new java.awt.event.ItemListener() {
+			public void itemStateChanged(java.awt.event.ItemEvent e) {
+				configureTable((String) comboBox.getSelectedItem());
+			}
+		});
+	}
+
+	private String getSelectedLanguageCode() {
+		String selectedElement = (String) comboBox.getSelectedItem();
+		String languageCode =
+			selectedElement.substring(
+				selectedElement.length() - 3,
+				selectedElement.length() - 1);
+		return languageCode;
+	}
+
+	/**
+	 * Generates button listeners
+	 */
+	private void generateButtonListeners() {
+		saveButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String languageCode = getSelectedLanguageCode();
+				String content =
+					LanguageFile.generateFile(tableModel, languageCode);
+				if (languageCode.equals("en")) {
+					WriteAccess.writeFile(content, new File("LangRes.java"));
+				} else {
+					WriteAccess.writeFile(
+						content,
+						new File("LangRes_" + languageCode + ".java"));
+				}
+			}
+		});
+		loadButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loadLanguageFile();
+			}
+		});
+	}
+
+	private void loadLanguageFile() {
+		final JFileChooser fc = new JFileChooser(new File("."));
+		fc.setDialogTitle(
+			"Select " + getSelectedLanguageCode() + " language file");
+		fc.setFileHidingEnabled(true);
+		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fc.setMultiSelectionEnabled(false);
+
+		int returnVal = fc.showOpenDialog(this);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fc.getSelectedFile();
+			if (file != null) {
+				if (!file.isDirectory()) {
+					LanguageFile.readLanguageFile(tableModel, file);
+				}
+			}
+		}
+	}
 
 }
