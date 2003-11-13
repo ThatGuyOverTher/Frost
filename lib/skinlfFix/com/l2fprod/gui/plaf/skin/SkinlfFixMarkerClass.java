f084c5a85adeffe1b11656171b3851c48583ee7f
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2003-11-13 00:05:31 +0000

    Just to check if the skinlfFix.jar exists

---
+++ lib/skinlfFix/com/l2fprod/gui/plaf/skin/SkinlfFixMarkerClass.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Nov 13, 2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.l2fprod.gui.plaf.skin;
+
+/**
+ * Not much here. Just to check that the skinlfFix.jar exists
+ */
+public class SkinlfFixMarkerClass {
+
+	/**
+	 * Not much here. Just to check that the skinlfFix.jar exists
+	 */
+	public SkinlfFixMarkerClass() {
+		super();
+	}
+
+}
