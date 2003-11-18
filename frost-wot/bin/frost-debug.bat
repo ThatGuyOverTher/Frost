5272c36f9ce1b848bf85c91b346b98871ece2888
zlatinb <zlatinb@unknown>
2003-11-18 19:00:05 +0000

    enable assertions, change some names

---
+++ frost-wot/bin/frost-debug.bat
@@ -0,0 +1,4 @@
+java -ea -cp lib/fecImpl.jar;lib/genChkImpl.jar;lib/xercesImpl.jar;lib/xml-apis.jar;lib/BCastle.jar;lib/skinlfFix.jar;lib/skinlf.jar;.;frost.jar frost.frost %1 %2 %3 %4 %5 %6 %7
+rem This pause statement was added to enable you to check messages after frost exits
+pause
+
