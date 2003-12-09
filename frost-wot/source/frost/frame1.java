b52ab4d312d68a2407f060f0552e3bb5062d2106
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2003-12-09 22:25:37 +0000

    BoardInfoFrame can now change its language while it is showing (and a little bit of refactoring too)

--- frost-wot/source/frost/frame1.java
+++ frost-wot/source/frost/frame1.java
@@ -3020,8 +3020,7 @@ public class frame1 extends JFrame implements ClipboardOwner {
 
 	private void tofDisplayBoardInfoMenuItem_actionPerformed(ActionEvent e) {
 		if (BoardInfoFrame.isDialogShowing() == false) {
-			BoardInfoFrame boardInfo =
-				new BoardInfoFrame(this, languageResource.getResourceBundle());
+			BoardInfoFrame boardInfo = new BoardInfoFrame(this, languageResource);
 			boardInfo.startDialog();
 		}
 	}
