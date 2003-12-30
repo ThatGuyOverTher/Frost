10ff303833bfc9ab0b419fb930318dc7ed7ba621
zlatinb <zlatinb@unknown>
2003-12-30 03:39:46 +0000

    plug in the notifications

---
+++ frost-wot/source/frost/threads/NotifyByEmailThread.java
@@ -0,0 +1,16 @@
+package frost.threads;
+
+import java.util.Observer;
+import java.util.Observable;
+
+
+public class NotifyByEmailThread extends Thread implements Observer{
+
+	public void run() {
+		throw new UnsupportedOperationException("not implemented yet. Come back later");
+	}
+
+	public void update(Observable o,Object arg) {
+		throw new UnsupportedOperationException("not implemented yet. Come back later");
+	}
+}
