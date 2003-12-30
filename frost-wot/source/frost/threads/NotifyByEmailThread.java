cd3ad89cb3b6bc29c874927000384691f85e3fc7
zlatinb <zlatinb@unknown>
2003-12-30 22:37:56 +0000

    work

--- frost-wot/source/frost/threads/NotifyByEmailThread.java
+++ frost-wot/source/frost/threads/NotifyByEmailThread.java
@@ -3,19 +3,39 @@ package frost.threads;
 import java.util.*;
 
 import frost.gui.objects.FrostDownloadItemObject;
-import frost.Core;
+import frost.*;
+import java.io.File;
 
-
+/**
+ * 
+ * @author zlatinb
+ *
+ * This thread does very simple email notifications for completed files.
+ * I deliberately do not want to make it too powerful of customizable.
+ */
 public class NotifyByEmailThread extends Thread implements Observer{
 	
 	private static String template; //TODO: load message body here
 	private static final String nameTag = "<filename>"; 
+	private static final String subject = "[Frost] Download finished";
+	
+	private static String SMTPServer, SMTPuser, SMTPpass;
 	
 	private LinkedList queue;
 	
 	public NotifyByEmailThread() {
 		queue = new LinkedList();
 		setDaemon(true);
+		
+		File body = new File("email");
+		if (body.exists() && body.isFile() && body.length() >0 && body.length() < 64*1024)
+			template = FileAccess.read(body.getPath());
+		else {
+			template = "Hello\n, this is the automatic Frost email notifier. "+
+							"The download of file <filename> has completed.\n\n";
+			FileAccess.writeFile(template,body);
+		}
+		
 	}
 
 	public void run() {
@@ -40,6 +60,7 @@ public class NotifyByEmailThread extends Thread implements Observer{
 		}
 	}
 
+
 	public void update(Observable o,Object arg) {
 		
 		assert o instanceof FrostDownloadItemObject &&
@@ -51,4 +72,40 @@ public class NotifyByEmailThread extends Thread implements Observer{
 		}
 			
 	}
+	
+	/**
+	 * @return
+	 */
+	public static String getTemplate() {
+		return template;
+	}
+
+	/**
+	 * @param string
+	 */
+	public static void setSMTPpass(String string) {
+		SMTPpass = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public static void setSMTPServer(String string) {
+		SMTPServer = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public static void setSMTPuser(String string) {
+		SMTPuser = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public static void setTemplate(String string) {
+		template = string;
+	}
+
 }
