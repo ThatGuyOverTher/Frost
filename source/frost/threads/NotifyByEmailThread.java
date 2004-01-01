04f6afd3d7e7c525e4cb0042ff870e791c993a1b
zlatinb <zlatinb@unknown>
2004-01-01 06:42:17 +0000

    work

--- source/frost/threads/NotifyByEmailThread.java
+++ source/frost/threads/NotifyByEmailThread.java
@@ -6,149 +6,157 @@ import frost.gui.objects.FrostDownloadItemObject;
 import frost.*;
 import java.io.File;
 
-
 import javax.mail.*;
 import javax.mail.internet.*;
 import com.sun.mail.smtp.*;
 
 /**
- * 
  * @author zlatinb
- *
- * This thread does very simple email notifications for completed files.
- * I deliberately do not want to make it too powerful of customizable.
+ * 
+ * This thread does very simple email notifications for completed files. I
+ * deliberately do not want to make it too powerful of customizable.
  */
-public class NotifyByEmailThread extends Thread implements Observer{
-	
-	private static String template; //TODO: load message body here
-	private static final String nameTag = "<filename>"; 
+public class NotifyByEmailThread extends Thread implements Observer {
+
+	private static String template; 
+	private static final String nameTag = "<filename>";
 	private static final String subject = "[Frost] Download finished";
-	
+
 	private static String SMTPServer, SMTPuser, SMTPpass, notifyAddress;
-	
+
 	private LinkedList queue;
-	
+
 	private SMTPMessage message;
 	private InternetAddress address;
 	private Session session;
 	private Properties sessionProperties;
 	private SMTPTransport transport;
-	
+
 	public NotifyByEmailThread() throws Error {
 		queue = new LinkedList();
 		setDaemon(true);
-		
+
 		//init the body of the message
 		File body = new File("email");
-		if (body.exists() && body.isFile() && body.length() >0 && body.length() < 64*1024)
+		if (body.exists()
+			&& body.isFile()
+			&& body.length() > 0
+			&& body.length() < 64 * 1024)
 			template = FileAccess.read(body.getPath());
 		else {
-			template = "Hello\n, this is the automatic Frost email notifier. "+
-							"The download of file <filename> has completed.\n\n";
-			FileAccess.writeFile(template,body);
-			
-		//get the settings
-			SMTPServer = Core.frostSettings.getValue("SMTP.server");
-			SMTPuser = Core.frostSettings.getValue("SMTP.user");
-			SMTPpass = Core.frostSettings.getValue("SMTP.pass");
-			notifyAddress = Core.frostSettings.getValue("notifyAddress");
-			
-			if (SMTPServer == null || SMTPuser == null || SMTPpass == null || notifyAddress ==null ||
-					SMTPServer.length()==0 || SMTPuser.length()==0 || SMTPpass.length()==0 || notifyAddress.length()==0)
-				throw new Error ("invalid SMTP parameters");
-		//init the session properties
-			sessionProperties = new Properties();
-			sessionProperties.setProperty("mail.host",SMTPServer);
-			sessionProperties.setProperty("mail.user",SMTPpass);
-			session = Session.getInstance(sessionProperties);
-			transport = new SMTPTransport(session, new URLName("frostNotifier"));
-			
-			try{
-				address = new InternetAddress(notifyAddress);
-				address.validate();
-			}catch(AddressException e){
-				throw new Error("invalid email address");
-			}
+			template =
+				"Hello\n, this is the automatic Frost email notifier. "
+					+ "The download of file <filename> has completed.\n\n";
+			FileAccess.writeFile(template, body);
 		}
-		
+
+		//get the settings
+		SMTPServer = Core.frostSettings.getValue("SMTP.server");
+		SMTPuser = Core.frostSettings.getValue("SMTP.user");
+		SMTPpass = Core.frostSettings.getValue("SMTP.pass");
+		notifyAddress = Core.frostSettings.getValue("notifyAddress");
+
+		if (SMTPServer == null
+			|| SMTPuser == null
+			|| SMTPpass == null
+			|| notifyAddress == null
+			|| SMTPServer.length() == 0
+			|| SMTPuser.length() == 0
+			|| SMTPpass.length() == 0
+			|| notifyAddress.length() == 0)
+			throw new Error("invalid SMTP parameters");
+		//init the session properties
+		sessionProperties = new Properties();
+		sessionProperties.setProperty("mail.host", SMTPServer);
+		sessionProperties.setProperty("mail.user", SMTPpass);
+		session = Session.getInstance(sessionProperties);
+		transport = new SMTPTransport(session, new URLName("frostNotifier"));
+
+		try {
+			address = new InternetAddress(notifyAddress);
+			address.validate();
+		} catch (AddressException e) {
+			throw new Error("invalid email address");
+		}
+
 	}
 
 	public void run() {
-		
+
 		String currentName;
-		running: while(true) {
-		
-			synchronized(queue) {
-				 if (queue.size()==0)
+		running : while (true) {
+
+			synchronized (queue) {
+				if (queue.size() == 0)
 					try {
 						queue.wait();
 						continue;
-					}catch(InterruptedException e){break running;}
-				else 
-					currentName = (String)queue.removeFirst();
+					} catch (InterruptedException e) {
+						break running;
+					} else
+					currentName = (String) queue.removeFirst();
 			}
-			Core.getOut().println("notifying for received file "+currentName);
-			
-			String messageBody = template.replaceAll(nameTag,currentName);
-			
+			Core.getOut().println("notifying for received file " + currentName);
+
+			String messageBody = template.replaceAll(nameTag, currentName);
+
 			//check if we're connected, and if not connect
-			
+
 			if (!transport.isConnected())
-				try{
-					transport.connect(SMTPServer,SMTPuser,SMTPpass);
-				}catch(MessagingException e){
+				try {
+					transport.connect(SMTPServer, SMTPuser, SMTPpass);
+				} catch (MessagingException e) {
 					Core.getOut().println("couldn't connect to smtp server");
 					e.printStackTrace(Core.getOut());
-					//TODO:decide what to do.  I think just wait 5 mins and try again?
-					mixed.wait(5*60*1000);
+					//TODO:decide what to do. I think just wait 5 mins and try
+					// again?
+					mixed.wait(5 * 60 * 1000);
 					queue.addFirst(currentName);
 					continue running;
 				}
-			
+
 			//compose the message
-				message = new SMTPMessage(session);
-				message.setNotifyOptions(SMTPMessage.NOTIFY_NEVER);
-				message.setReturnOption(SMTPMessage.NOTIFY_NEVER);
-				try{
-					message.setEnvelopeFrom("Frost");
-					message.setSubject(subject);
-					message.setText(messageBody);
-					
-					//now send the message
-					transport.sendMessage(message, new Address[]{address});
-					Core.getOut().println("message sent successfully");
-					
-					//check if there are more messages to send, and if not disconnect
-					if (queue.size()==0)
-						transport.close();
-				}catch (SendFailedException e){
-					e.printStackTrace(Core.getOut());
-					//wait 5 mins, retry
-					mixed.wait(5*60*1000);
-					queue.addFirst(currentName);
-					continue running;
-				}catch(MessagingException e){
-					//we screwed up somehow..
-					e.printStackTrace(Core.getOut());
-				} 
-				
-					
+			message = new SMTPMessage(session);
+			message.setNotifyOptions(SMTPMessage.NOTIFY_NEVER);
+			message.setReturnOption(SMTPMessage.NOTIFY_NEVER);
+			try {
+				message.setEnvelopeFrom("Frost");
+				message.setSubject(subject);
+				message.setText(messageBody);
+
+				//now send the message
+				transport.sendMessage(message, new Address[] { address });
+				Core.getOut().println("message sent successfully");
+
+				//check if there are more messages to send, and if not
+				// disconnect
+				if (queue.size() == 0)
+					transport.close();
+			} catch (SendFailedException e) {
+				e.printStackTrace(Core.getOut());
+				//wait 5 mins, retry
+				mixed.wait(5 * 60 * 1000);
+				queue.addFirst(currentName);
+				continue running;
+			} catch (MessagingException e) {
+				//we screwed up somehow..
+				e.printStackTrace(Core.getOut());
+			}
+
 		}
 	}
 
+	public void update(Observable o, Object arg) {
 
-	public void update(Observable o,Object arg) {
-		
-		assert o instanceof FrostDownloadItemObject &&
-			arg instanceof String : "incorrect parameters at email notifier";
-		
-		synchronized(queue){
+		assert o instanceof FrostDownloadItemObject
+			&& arg instanceof String : "incorrect parameters at email notifier";
+
+		synchronized (queue) {
 			queue.add(arg);
 			queue.notifyAll();
 		}
-			
+
 	}
-	
 
 	/**
 	 * @return
