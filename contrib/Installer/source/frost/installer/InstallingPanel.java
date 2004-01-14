c5d3afb2177898ae1210b4c6de66224b61c1604f
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-01-14 21:09:28 +0000

    installApplication method completed. Visual feedback for the user while installing added.

--- contrib/Installer/source/frost/installer/InstallingPanel.java
+++ contrib/Installer/source/frost/installer/InstallingPanel.java
@@ -6,6 +6,7 @@
  */
 package frost.installer;
 
+import java.applet.AppletContext;
 import java.awt.LayoutManager;
 import java.awt.event.*;
 import java.awt.event.ActionListener;
@@ -13,6 +14,8 @@ import java.io.*;
 import java.io.File;
 import java.net.*;
 import java.net.URL;
+import java.util.*;
+import java.util.Properties;
 
 import javax.swing.JPanel;
 
@@ -23,6 +26,7 @@ public class InstallingPanel extends JPanel {
 	
 	private InstallerApplet applet;
 	private URL jnlpRemoteLocation = null;
+	private URL dtdLocation = null;
 	private File jnlpLocalDirectory = null;
 
 	private javax.swing.JPanel topPanel = null;
@@ -30,6 +34,10 @@ public class InstallingPanel extends JPanel {
 	private javax.swing.JLabel jLabel = null;
 	private javax.swing.JLabel jLabel1 = null;
 	private javax.swing.JButton backButton = null;
+	private javax.swing.JTextArea messagesTextArea = null;
+	private javax.swing.JScrollPane jScrollPane = null;
+	private javax.swing.JProgressBar jProgressBar = null;
+	private javax.swing.JLabel outputFolderLabel = null;
 	/**
 	 * 
 	 */
@@ -102,15 +110,38 @@ public class InstallingPanel extends JPanel {
 		if(bottomPanel == null) {
 			bottomPanel = new javax.swing.JPanel();
 			java.awt.GridBagConstraints consGridBagConstraints3 = new java.awt.GridBagConstraints();
+			java.awt.GridBagConstraints consGridBagConstraints6 = new java.awt.GridBagConstraints();
+			java.awt.GridBagConstraints consGridBagConstraints1 = new java.awt.GridBagConstraints();
+			java.awt.GridBagConstraints consGridBagConstraints2 = new java.awt.GridBagConstraints();
+			consGridBagConstraints1.fill = java.awt.GridBagConstraints.HORIZONTAL;
+			consGridBagConstraints1.weightx = 1.0;
+			consGridBagConstraints1.gridx = 0;
+			consGridBagConstraints1.gridy = 1;
+			consGridBagConstraints1.gridwidth = 2;
+			consGridBagConstraints1.insets = new java.awt.Insets(0,25,5,25);
+			consGridBagConstraints2.gridy = 0;
+			consGridBagConstraints2.gridx = 0;
+			consGridBagConstraints2.insets = new java.awt.Insets(10,25,5,25);
+			consGridBagConstraints2.fill = java.awt.GridBagConstraints.HORIZONTAL;
+			consGridBagConstraints6.fill = java.awt.GridBagConstraints.BOTH;
+			consGridBagConstraints6.weighty = 1.0;
+			consGridBagConstraints6.weightx = 1.0;
+			consGridBagConstraints6.insets = new java.awt.Insets(5,25,15,25);
+			consGridBagConstraints6.gridwidth = 2;
+			consGridBagConstraints6.gridx = 0;
+			consGridBagConstraints6.gridy = 3;
 			consGridBagConstraints3.ipady = 0;
 			consGridBagConstraints3.ipadx = 0;
-			consGridBagConstraints3.gridy = 3;
+			consGridBagConstraints3.gridy = 4;
 			consGridBagConstraints3.gridx = 0;
 			consGridBagConstraints3.anchor = java.awt.GridBagConstraints.EAST;
-			consGridBagConstraints3.insets = new java.awt.Insets(6,0,6,10);
+			consGridBagConstraints3.insets = new java.awt.Insets(6,0,6,30);
 			consGridBagConstraints3.weightx = 1.0D;
 			bottomPanel.setLayout(new java.awt.GridBagLayout());
 			bottomPanel.add(getBackButton(), consGridBagConstraints3);
+			bottomPanel.add(getJScrollPane(), consGridBagConstraints6);
+			bottomPanel.add(getJProgressBar(), consGridBagConstraints1);
+			bottomPanel.add(getOutputFolderLabel(), consGridBagConstraints2);
 			bottomPanel.setBackground(new java.awt.Color(236,233,216));
 			bottomPanel.setBorder(javax.swing.BorderFactory.createMatteBorder(1,0,0,0,java.awt.Color.white));
 		}
@@ -173,21 +204,111 @@ public class InstallingPanel extends JPanel {
 	 */
 	void installApplication() {
 		getBackButton().setEnabled(false);
+		getOutputFolderLabel().setText("Output folder: " + jnlpLocalDirectory.toString());
+		JnlpFile jnlpFile = new JnlpFile();
+		jnlpFile.setRemoteLocation(jnlpRemoteLocation);
+		jnlpFile.setDtdLocation(dtdLocation);
 		try {
-			URLConnection connection = jnlpRemoteLocation.openConnection();
-			Object content = jnlpRemoteLocation.getContent();
-			System.out.println("Content: " + content.toString());
-		} catch (IOException exception) {
-			System.out.println("Exception: " + exception.getMessage());
+			getMessagesTextArea().append("Downloading jnlp file... ");
+			getJProgressBar().setValue(10);
+			jnlpFile.download();
+			getMessagesTextArea().append("Done\nParsing document... ");
+			getJProgressBar().setValue(30);
+			jnlpFile.parseDocument();
+			getMessagesTextArea().append("Done\nChecking validity... ");
+			getJProgressBar().setValue(40);
+			if (jnlpFile.isValid()) {
+				getMessagesTextArea().append("Done\nReplacing codebase... ");
+				getJProgressBar().setValue(50);
+				jnlpFile.replaceCodebase(jnlpLocalDirectory.toURL());
+				getMessagesTextArea().append("Done\nWriting local copy... ");
+				getJProgressBar().setValue(60);
+				File localFile = jnlpFile.writeToLocalDirectory(jnlpLocalDirectory);	
+				getMessagesTextArea().append("Done\nLaunching Java Web Start... ");
+				getJProgressBar().setValue(70);
+				applet.getAppletContext().showDocument(localFile.toURL());
+			} else {
+				//TODO: let the user try again
+				getMessagesTextArea().append("Not Done \nThe document is not valid");
+			}
+		} catch (Exception exception) {
+			//TODO: let the user try again and deal with timeouts (downloading)
+			getMessagesTextArea().append("Exception: \n" + exception.getMessage());
 		}
-
 	}
+	
+	/**
+	 * 
+	 */
+	void clear() {
+		getJProgressBar().setValue(0);
+		getMessagesTextArea().setText("");
+	}
+	
+	
 
 	/**
 	 * @param file
 	 */
 	void setJnlpLocalDirectory(File file) {
 		jnlpLocalDirectory = file;
+	}
+
+	/**
+	 * This method initializes messagesTextArea
+	 * 
+	 * @return javax.swing.JTextArea
+	 */
+	private javax.swing.JTextArea getMessagesTextArea() {
+		if(messagesTextArea == null) {
+			messagesTextArea = new javax.swing.JTextArea();
+		}
+		return messagesTextArea;
+	}
+	/**
+	 * This method initializes jScrollPane
+	 * 
+	 * @return javax.swing.JScrollPane
+	 */
+	private javax.swing.JScrollPane getJScrollPane() {
+		if(jScrollPane == null) {
+			jScrollPane = new javax.swing.JScrollPane();
+			jScrollPane.setViewportView(getMessagesTextArea());
+		}
+		return jScrollPane;
+	}
+
+	/**
+	 * @param url
+	 */
+	void setDtdLocation(URL url) {
+		dtdLocation = url;
+	}
+
+	/**
+	 * This method initializes jProgressBar
+	 * 
+	 * @return javax.swing.JProgressBar
+	 */
+	private javax.swing.JProgressBar getJProgressBar() {
+		if(jProgressBar == null) {
+			jProgressBar = new javax.swing.JProgressBar();
+			jProgressBar.setStringPainted(true);
+		}
+		return jProgressBar;
+	}
+	/**
+	 * This method initializes outputFolderLabel
+	 * 
+	 * @return javax.swing.JLabel
+	 */
+	private javax.swing.JLabel getOutputFolderLabel() {
+		if(outputFolderLabel == null) {
+			outputFolderLabel = new javax.swing.JLabel();
+			outputFolderLabel.setText("Output folder:");
+			outputFolderLabel.setFont(new java.awt.Font("Dialog", java.awt.Font.PLAIN, 12));
+		}
+		return outputFolderLabel;
 	}
 
 }
