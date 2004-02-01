5ea0f3b53dc80dd18853b7c7e61278f36624b35e
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-02-01 12:37:42 +0000

    Removed: frost.log output redirection. Not necessary any more.

--- frost-wot/bin/frost.sh
+++ frost-wot/bin/frost.sh
@@ -2,4 +2,4 @@
 JARDIR=.
 CLASSPATH="$JARDIR/frost.jar:$JARDIR/lib/fecImpl.jar:$JARDIR/lib/genChkImpl.jar:$JARDIR/lib/xercesImpl.jar:$JARDIR/lib/xml-apis.jar:$JARDIR/lib/skinlfFix.jar:$JARDIR/lib/skinlf.jar:$JARDIR/lib/BCastle.jar:$CLASSPATH"
 export CLASSPATH
-java frost.frost "$@" > frost.log &
+java frost.frost "$@"
