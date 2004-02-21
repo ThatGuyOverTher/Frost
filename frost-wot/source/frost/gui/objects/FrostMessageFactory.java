d9843300b360d9d21c05169d362267cdd7cf7d21
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-02-21 15:10:03 +0000

    Initial version

---
+++ frost-wot/source/frost/gui/objects/FrostMessageFactory.java
@@ -0,0 +1,86 @@
+package frost.gui.objects;
+
+import java.io.File;
+import java.util.logging.*;
+
+import org.shiftone.cache.*;
+
+/**
+ * There is a single instance of the class <code>FrostmessageFactory</code>,
+ * accessed through the static method {@link #getInstance()}.
+ *
+ * @pattern Singleton
+ *
+ * @author Administrator
+ *
+ * @version $Revision$
+ */
+public class FrostMessageFactory {
+
+	private static final String CACHE_NAME = "fifo";
+	private static final int CACHE_SIZE = 500;	//TODO: Add this value to the preferences
+	private static final int CACHE_TIMEOUT = Integer.MAX_VALUE;
+
+	private static Logger logger = Logger.getLogger(FrostMessageFactory.class.getName());
+
+	private Cache cache = null;
+
+	/**
+	 * The unique instance of this class.
+	 */
+	private static FrostMessageFactory instance = null;
+
+	/**
+	 * Prevent instances of this class from being created.
+	 */
+	private FrostMessageFactory() {
+		try {
+			cache =
+				new CacheConfiguration().createConfiguredCache(
+					CACHE_NAME,
+					CACHE_TIMEOUT,
+					CACHE_SIZE);
+		} catch (Exception exception) {
+			logger.log(
+				Level.SEVERE,
+				"Error while creating the object cache. Object caching will not be used.",
+				exception);
+		}
+	}
+
+	/**
+	 * Return the unique instance of this class.
+	 *
+	 * @return the unique instance of this class
+	 */
+	public static FrostMessageFactory getInstance() {
+		if (instance == null) {
+			instance = new FrostMessageFactory();
+		}
+		return instance;
+	}
+
+	public static FrostMessageObject createFrostMessageObject(File messageFile) throws Exception {
+		return getInstance().innerCreateFrostMessageObject(messageFile);
+	}
+
+	/**
+	 * @param messageFile
+	 * @return
+	 */
+	private FrostMessageObject innerCreateFrostMessageObject(File messageFile) throws Exception {
+		if (cache != null) {
+			Object messageObject = cache.getObject(messageFile);
+			if (messageObject != null) {
+				return (FrostMessageObject) messageObject;
+			} else {
+				messageObject = new FrostMessageObject(messageFile);
+				cache.addObject(messageFile, messageObject);
+				return (FrostMessageObject) messageObject;
+			}
+		} else {
+			return new FrostMessageObject(messageFile);
+		}
+	}
+
+}
