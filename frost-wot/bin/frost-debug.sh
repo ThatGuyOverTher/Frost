8d14b3df6da5afbfbbe9f9769822f9e1af8b62c2
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-03-01 18:46:07 +0000

    Changed: the classpath is got from the manifest now

--- frost-wot/bin/frost-debug.sh
+++ frost-wot/bin/frost-debug.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-JARDIR=.
-CLASSPATH="$JARDIR/frost.jar:$JARDIR/lib/fecImpl.jar:$JARDIR/lib/genChkImpl.jar:$JARDIR/lib/xercesImpl.jar:$JARDIR/lib/xml-apis.jar:$JARDIR/lib/skinlfFix.jar:$JARDIR/lib/skinlf.jar:$JARDIR/lib/BCastle.jar:$CLASSPATH"
-export CLASSPATH
-java -ea frost.frost "$@"
+java -ea -jar frost.jar "$@"
