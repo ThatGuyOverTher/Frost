25a384340d10701ada1af9be97ae1acddf455301
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-04-02 10:31:45 +0000

    Refactoring: configureButton method added (I will remove it from some other classes) Changed: code layout improved.

--- source/frost/gui/components/MiscToolkit.java
+++ source/frost/gui/components/MiscToolkit.java
@@ -7,26 +7,65 @@
 package frost.gui.components;
 
 import java.awt.*;
-import java.util.Collection;
+import java.util.*;
+import java.util.logging.Logger;
 
 import javax.swing.*;
 
 /**
  * @author Administrator
  *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ * This a gui related utilities class.
  */
 public class MiscToolkit {
-	
-	private static MiscToolkit instance = null;
-	
+
+	private static MiscToolkit instance = new MiscToolkit();
+
+	private static Logger logger = Logger.getLogger(MiscToolkit.class.getName());
+	/**
+	 * Return the unique instance of this class.
+	 *
+	 * @return the unique instance of this class
+	 */
+	public static MiscToolkit getInstance() {
+		return instance;
+	}
+
 	/**
 	 * Prevent instances of this class from being created.
 	 */
 	private MiscToolkit() {
 	}
-	
+
+	/**
+	 * Configures a button, setting its tooltip text, rollover icon and some other
+	 * default properties.
+	 * @param button the button to configure
+	 * @param toolTipKey language resource key to extract its tooltip text with
+	 * @param rolloverIcon displayed icon when mouse arrow is over button
+	 * @param languageResource language resource key to extract its tooltip text from
+	 */
+	public void configureButton(
+		JButton button,
+		String toolTipKey,
+		String rolloverIcon,
+		ResourceBundle languageResource) {
+
+		String text = null;
+		try {
+			text = languageResource.getString(toolTipKey);
+		} catch (MissingResourceException ex) {
+			logger.severe("Missing resource in configureButton method: " + toolTipKey);
+			text = toolTipKey; // better than nothing ;)
+		}
+		button.setToolTipText(text);
+
+		button.setRolloverIcon(new ImageIcon(getClass().getResource(rolloverIcon)));
+		button.setMargin(new Insets(0, 0, 0, 0));
+		button.setBorderPainted(false);
+		button.setFocusPainted(false);
+	}
+
 	/**
 	 * This method enables/disables the subcomponents of a container.
 	 * If the container contains other containers, the subcomponents of those
@@ -38,31 +77,14 @@ public class MiscToolkit {
 		int componentCount = container.getComponentCount();
 		for (int x = 0; x < componentCount; x++) {
 			Component component = container.getComponent(x);
-			if (component instanceof Container) { 
-				setContainerEnabledInner((Container)component, enabled);
+			if (component instanceof Container) {
+				setContainerEnabledInner((Container) component, enabled);
 			} else {
 				component.setEnabled(enabled);
 			}
 		}
 	}
-	
-	/**
-	 * @param container
-	 * @param enabled
-	 */
-	private void setContainerEnabledInner(Container container, boolean enabled) {
-			int componentCount = container.getComponentCount();
-			for (int x = 0; x < componentCount; x++) {
-				Component component = container.getComponent(x);
-				if (component instanceof Container) { 
-					setContainerEnabledInner((Container)component, enabled);
-				} else {
-					component.setEnabled(enabled);
-				}
-			} 
-			container.setEnabled(enabled);
-		}
-	
+
 	/**
 	 * This method enables/disables the subcomponents of a container.
 	 * If the container contains other containers, the components of those
@@ -85,27 +107,48 @@ public class MiscToolkit {
 			}
 		}
 	}
-	
+
+	/**
+	 * @param container
+	 * @param enabled
+	 */
+	private void setContainerEnabledInner(Container container, boolean enabled) {
+		int componentCount = container.getComponentCount();
+		for (int x = 0; x < componentCount; x++) {
+			Component component = container.getComponent(x);
+			if (component instanceof Container) {
+				setContainerEnabledInner((Container) component, enabled);
+			} else {
+				component.setEnabled(enabled);
+			}
+		}
+		container.setEnabled(enabled);
+	}
+
 	/**
 	 * @param container
 	 * @param enabled
 	 * @param exceptions
 	 */
-	private void setContainerEnabledInner(Container container, boolean enabled, Collection exceptions) {
-			int componentCount = container.getComponentCount();
-			for (int x = 0; x < componentCount; x++) {
-				Component component = container.getComponent(x);
-				if (!exceptions.contains(component)) {
-					if (component instanceof Container) { 
-						setContainerEnabledInner((Container) component, enabled, exceptions);
-					} else {
-						component.setEnabled(enabled);
-					}
+	private void setContainerEnabledInner(
+		Container container,
+		boolean enabled,
+		Collection exceptions) {
+			
+		int componentCount = container.getComponentCount();
+		for (int x = 0; x < componentCount; x++) {
+			Component component = container.getComponent(x);
+			if (!exceptions.contains(component)) {
+				if (component instanceof Container) {
+					setContainerEnabledInner((Container) component, enabled, exceptions);
+				} else {
+					component.setEnabled(enabled);
 				}
 			}
-			container.setEnabled(enabled);
 		}
-		
+		container.setEnabled(enabled);
+	}
+
 	/**
 	 * This method shows a message in a JDialog
 	 * @param message the message to show
@@ -116,24 +159,13 @@ public class MiscToolkit {
 		JOptionPane optionPane = new JOptionPane(message, type);
 		JFrame frame = new JFrame();
 		frame.setTitle("Frost");
-		java.awt.Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(dimension.width / 2, dimension.height / 2);
 		frame.setUndecorated(true);
-		frame.show(); 
+		frame.show();
 		frame.toFront();
 		optionPane.createDialog(frame, title).show();
-		frame.dispose();	
-	}
-	/**
-	 * Return the unique instance of this class.
-	 *
-	 * @return the unique instance of this class
-	 */
-	public synchronized static MiscToolkit getInstance() {
-		if (instance == null) {
-			instance = new MiscToolkit();	
-		}
-		return instance;
+		frame.dispose();
 	}
 
 }
