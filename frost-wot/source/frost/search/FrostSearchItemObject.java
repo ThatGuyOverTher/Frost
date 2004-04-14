637b211f55fac97b6d98e287a03ef3a14394f73a
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-04-14 08:25:20 +0000

    Added: internationalized

--- frost-wot/source/frost/search/FrostSearchItemObject.java
+++ frost-wot/source/frost/search/FrostSearchItemObject.java
@@ -2,15 +2,18 @@ package frost.search;
 
 import frost.gui.model.TableMember;
 import frost.gui.objects.FrostBoardObject;
+import frost.gui.translation.UpdatingLanguageResource;
 import frost.messages.*;
-import frost.search.*;
 
 
 public class FrostSearchItemObject implements FrostSearchItem, TableMember
 {
-    FrostBoardObject board;
-    SharedFileObject sfo;
-    int state;
+    private String offlineLocalized;
+	private String anonymousLocalized;
+	private UpdatingLanguageResource languageResource;
+	private FrostBoardObject board;
+	private SharedFileObject sfo;
+	private int state;
 
     public static final int STATE_NONE        = 1; // set if a search table item is only in search table
     public static final int STATE_DOWNLOADED  = 2; // set if the item is already downloaded and is found in download folder
@@ -18,36 +21,51 @@ public class FrostSearchItemObject implements FrostSearchItem, TableMember
     public static final int STATE_UPLOADING   = 4; // set if file is in upload table
     public static final int STATE_OFFLINE     = 5; // set if file is offline
 
-    public FrostSearchItemObject( FrostBoardObject board, SharedFileObject key, int state )
-    {
-        this.board = board;
-        this.sfo = key;
-        this.state = state;
-    }
+	public FrostSearchItemObject(
+		UpdatingLanguageResource newLanguageResource,
+		FrostBoardObject newBoard,
+		SharedFileObject newKey,
+		int newState) {
+			
+		languageResource = newLanguageResource;
+		board = newBoard;
+		sfo = newKey;
+		state = newState;
+		
+		anonymousLocalized = languageResource.getString("FrostSearchItemObject.Anonymous");
+		offlineLocalized = languageResource.getString("FrostSearchItemObject.Offline");
+	}
 
-    /**
-     * Returns the object representing value of column. Can be string or icon
-     *
-     * @param   column  Column to be displayed
-     * @return  Object representing table entry.
-     */
-    public Object getValueAt(int column)
-    {
-        // NEVER add <html> here, add a state or method like isOffline
-        // and do it the right way in SearchTable cellRenderer !!!
-        switch(column) {
-            case 0: return sfo.getFilename();
-            case 1: return sfo.getSize();
-            case 2: if( getState() == STATE_OFFLINE )  return "offline";
-		            else  return sfo.getDate();
-            case 3: if (sfo.getOwner()==null || sfo.getOwner().length()==0)
-	    		      return "Anonymous";
-		            else
-			          return sfo.getOwner();
-            case 4: return board.toString();
-            default: return "*ERR*";
-        }
-    }
+	/**
+	 * Returns the object representing value of column. Can be string or icon
+	 *
+	 * @param   column  Column to be displayed
+	 * @return  Object representing table entry.
+	 */
+	public Object getValueAt(int column) {
+		// NEVER add <html> here, add a state or method like isOffline
+		// and do it the right way in SearchTable cellRenderer !!!
+		switch (column) {
+			case 0 :
+				return sfo.getFilename();
+			case 1 :
+				return sfo.getSize();
+			case 2 :
+				if (getState() == STATE_OFFLINE)
+					return offlineLocalized;
+				else
+					return sfo.getDate();
+			case 3 :
+				if (sfo.getOwner() == null || sfo.getOwner().length() == 0)
+					return anonymousLocalized;
+				else
+					return sfo.getOwner();
+			case 4 :
+				return board.toString();
+			default :
+				return languageResource.getString("FrostSearchItemObject.*ERROR*");
+		}
+	}
     
     public int compareTo( TableMember anOther, int tableColumIndex )
     {
