18b53e5d83a43085f8599ecfa0caeabddc1cb93a
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-04-15 08:16:11 +0000

    Refactoring: all file downloading related classes moved to a new package (frost.fileTransfer.download)

---
+++ frost-wot/source/frost/fileTransfer/download/DownloadManager.java
@@ -0,0 +1,25 @@
+/*
+ * Created on Apr 15, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package frost.fileTransfer.download;
+
+/**
+ * @author Administrator
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class DownloadManager {
+
+	/**
+	 * 
+	 */
+	public DownloadManager() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
