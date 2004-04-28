ce583dea270d4c48b52c666124759339b4e4aad8
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-04-28 20:52:06 +0000

    Refactoring: instances of this class are now created by the UploadTicker Refactoring: code structure cleaned up

--- source/frost/fileTransfer/upload/UploadThread.java
+++ source/frost/fileTransfer/upload/UploadThread.java
@@ -31,7 +31,9 @@ import frost.messages.*;
 
 public class UploadThread extends Thread
 {
-    private LocalIdentity myId;
+    private UploadTicker ticker;
+
+	private LocalIdentity myId;
 
 	static java.util.ResourceBundle LangRes = java.util.ResourceBundle.getBundle("res.LangRes")/*#BundleType=List*/;
     
@@ -59,244 +61,247 @@ public class UploadThread extends Thread
 
     FrostUploadItemObject uploadItem;
 
-    public void run()
-    {
-    	if (batchId==null) {
-    		Exception er = new Exception();
-    		er.fillInStackTrace();
+	public void run() {
+		if (batchId == null) {
+			Exception er = new Exception();
+			er.fillInStackTrace();
 			logger.log(Level.SEVERE, "Exception thrown in run()", er);
-    	}
-    	if (Core.getMyBatches().values().size()==0)
-    		Core.getMyBatches().put(batchId,batchId);
-    		
-        try
-        {
-            String lastUploadDate = null; // NEVER uploaded
-            boolean success = false;
-            String[] result = { "Error", "Error" };
-            String currentDate = DateFun.getExtendedDate();
-            boolean sign = frame1.frostSettings.getBoolValue("signUploads");
+		}
+		if (Core.getMyBatches().values().size() == 0) {
+			Core.getMyBatches().put(batchId, batchId);
+		}
+		boolean sign = frame1.frostSettings.getBoolValue("signUploads");
+		try {
+			switch (mode) {
+				case MODE_UPLOAD :
+					upload(sign);
+					ticker.releaseUploadingThread();
+					break;
+				case MODE_GENERATE_SHA1 :
+					generateSHA1(sign);
+					ticker.releaseGeneratingThread();
+					break;
+				case MODE_GENERATE_CHK :
+					generateCHK();
+					ticker.releaseGeneratingThread();
+					break;
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Exception thrown in run()", e);
+			switch (mode) {
+				case MODE_UPLOAD :
+					ticker.releaseUploadingThread();
+					break;
+				case MODE_GENERATE_SHA1 :
+					ticker.releaseGeneratingThread();
+					break;
+				case MODE_GENERATE_CHK :
+					ticker.releaseGeneratingThread();
+					break;
+			}
+		}
+		((UploadTableModel) frame1.getInstance().getUploadTable().getModel()).updateRow(uploadItem);
+	}
+    
+    
+	/**
+	 * 
+	 */
+	private void upload(boolean sign) { //real upload
+		
+		String lastUploadDate = null; // NEVER uploaded
+		boolean success = false;
+		String[] result = { "Error", "Error" };
+		String currentDate = DateFun.getExtendedDate();
+		
+		logger.info("Upload of " + file + " with HTL " + htl + " started.");
 
-            if( mode == MODE_UPLOAD )
-            { //real upload
-                logger.info("Upload of " + file + " with HTL " + htl + " started.");
-                synchronized (frame1.threadCountLock)
-                {
-                    frame1.activeUploadThreads++;
-                }
+		result = FcpInsert.putFile("CHK@", file, null,
+			// metadata
+			htl, true, // doRedirect
+		uploadItem); // provide the uploadItem to indicate that this upload is contained in table
 
-                result =
-                    FcpInsert.putFile(
-                        "CHK@",
-                        file,
-                        null,// metadata
-                        htl,
-                        true, // doRedirect
-                        uploadItem); // provide the uploadItem to indicate that this upload is contained in table
+		if (result[0].equals("Success") || result[0].equals("KeyCollision")) {
+			success = true;
+			logger.info("Upload of " + file + " was successful.");
+			uploadItem.setKey(result[1]);
+			lastUploadDate = currentDate;
+		} else {
+			logger.warning("Upload of " + file + " was NOT successful.");
+		}
 
-                if( result[0].equals("Success") ||
-                    result[0].equals("KeyCollision") )
-                {
-                    success = true;
-                    logger.info("Upload of " + file + " was successful.");
-                    uploadItem.setKey(result[1]);
-                    lastUploadDate = currentDate;
-                }
-                else
-                {
-                    logger.warning("Upload of " + file + " was NOT successful.");
-                }
+		// item uploaded (maybe)
+		// REDFLAG: but this also resets upload date of yesterday, ... !
+		if (lastUploadDate != null)
+			uploadItem.setLastUploadDate(lastUploadDate);
+		// if NULL then upload failed -> shows NEVER in table
 
-                // item uploaded (maybe)
-                // REDFLAG: but this also resets upload date of yesterday, ... !
-                if( lastUploadDate != null ) 
-                    uploadItem.setLastUploadDate(lastUploadDate);
-                // if NULL then upload failed -> shows NEVER in table
+		uploadItem.setState(this.nextState);
 
-                uploadItem.setState(this.nextState);
+		//now update the files.xml with the CHK
+		// REDFLAG: was this really intented to run even if upload failed?
+		//what was it before?
+		if (success == true) {
+			SharedFileObject current;
 
-                synchronized (frame1.threadCountLock)
-                {
-                    frame1.activeUploadThreads--;
-                }
+			if (uploadItem.getFileSize().longValue() > FcpInsert.smallestChunk) {
+				logger.fine("attaching redirect to file " + file.getName());
+				current = new FECRedirectFileObject();
+				FecSplitfile splitFile = new FecSplitfile(file);
+				if (!splitFile.uploadInit())
+					throw new Error("file was just uploaded, but .redirect missing!");
 
-                //now update the files.xml with the CHK
-                // REDFLAG: was this really intented to run even if upload failed?
-                //what was it before?
-                if( success == true )
-                {
-					SharedFileObject current;
-                	
-					if (uploadItem.getFileSize().longValue() > FcpInsert.smallestChunk ) {
-						logger.fine("attaching redirect to file " + file.getName());
-						current = new FECRedirectFileObject();
-						FecSplitfile splitFile = new FecSplitfile(file);
-						if (!splitFile.uploadInit()) 
-							throw new Error("file was just uploaded, but .redirect missing!");
-						
-						//create a splitfile redirect without progress information
-						splitFile.createRedirectFile(false);
-											
-						((FECRedirectFileObject)current).setRedirect(
-								new String(FileAccess.readByteArray(splitFile.getRedirectFile())));
-					}
-					else {
-						current = new SharedFileObject();
-						logger.fine("not attaching redirect");
-					}
-                	
-					
-                	
-                	current.setKey(uploadItem.getKey());
-                    if (sign)
-                        current.setOwner(myId.getUniqueName());
-                    current.setFilename(uploadItem.getFileName());
-                    current.setSHA1(uploadItem.getSHA1());
-                    current.setBatch(uploadItem.getBatch());
-                    current.setSize(uploadItem.getFileSize().longValue());
-                    current.setDate(lastUploadDate);
-                    current.setLastSharedDate(lastUploadDate);
-                    Index.addMine(current, board);
-                    Index.add(current, board);
-                }
-            }
-            else if( mode == MODE_GENERATE_SHA1 )
-            {
-                frame1.setGeneratingCHK(true);
+				//create a splitfile redirect without progress information
+				splitFile.createRedirectFile(false);
 
-                //if this is the first startup, put at least one batch in
-                /*   if (frame1.getMyBatches().values().size() == 0) //
-                   	frame1.getMyBatches().put(batchId,batchId);
-                   else {
-                   	//take the first patch we come upon
-                   }*/
+				((FECRedirectFileObject) current).setRedirect(
+					new String(FileAccess.readByteArray(splitFile.getRedirectFile())));
+			} else {
+				current = new SharedFileObject();
+				logger.fine("not attaching redirect");
+			}
 
-                if (fileIndex % batchSize == 0)
-                {
-					Core.getMyBatches().put(batchId, batchId);
-                    while (Core.getMyBatches().contains(batchId))
-                        batchId = (new Long(r.nextLong())).toString();
-					Core.getMyBatches().put(batchId, batchId);
-                }
+			current.setKey(uploadItem.getKey());
+			if (sign) {
+				current.setOwner(myId.getUniqueName());
+			}
+			current.setFilename(uploadItem.getFileName());
+			current.setSHA1(uploadItem.getSHA1());
+			current.setBatch(uploadItem.getBatch());
+			current.setSize(uploadItem.getFileSize().longValue());
+			current.setDate(lastUploadDate);
+			current.setLastSharedDate(lastUploadDate);
+			Index.addMine(current, board);
+			Index.add(current, board);
+		}
+	}
+    
+	/**
+	 * 
+	 */
+	private void generateSHA1(boolean sign) {
+		if (fileIndex % batchSize == 0) {
+			Core.getMyBatches().put(batchId, batchId);
+			while (Core.getMyBatches().contains(batchId))
+				batchId = (new Long(r.nextLong())).toString();
+			Core.getMyBatches().put(batchId, batchId);
+		}
 
-                long now = System.currentTimeMillis();
-                String SHA1 = Core.getCrypto().digest(file);
-                logger.fine("digest generated in " + (System.currentTimeMillis() - now) + "  " + SHA1);
+		long now = System.currentTimeMillis();
+		String SHA1 = Core.getCrypto().digest(file);
+		logger.fine("digest generated in " + (System.currentTimeMillis() - now) + "  " + SHA1);
 
-                //create new KeyClass
-                SharedFileObject newKey = new SharedFileObject();
-                newKey.setKey(null);
-                newKey.setDate(null);
-                newKey.setLastSharedDate(DateFun.getDate());
-                newKey.setSHA1(SHA1);
-                newKey.setFilename(destination);
-                newKey.setSize(file.length());
-                newKey.setBatch(batchId);
-                if (sign)
-                    newKey.setOwner(myId.getUniqueName());
+		//create new KeyClass
+		SharedFileObject newKey = new SharedFileObject();
+		newKey.setKey(null);
+		newKey.setDate(null);
+		newKey.setLastSharedDate(DateFun.getDate());
+		newKey.setSHA1(SHA1);
+		newKey.setFilename(destination);
+		newKey.setSize(file.length());
+		newKey.setBatch(batchId);
+		if (sign) {
+			newKey.setOwner(myId.getUniqueName());
+		}
 
-                //update the gui
-                uploadItem.setSHA1(SHA1);
-                uploadItem.setKey(null);
-                uploadItem.setLastUploadDate(null);
-                uploadItem.setBatch(batchId);
-                fileIndex++;
-                //add to index
-                Index.addMine(newKey, board);
-                Index.add(newKey, board);
+		//update the gui
+		uploadItem.setSHA1(SHA1);
+		uploadItem.setKey(null);
+		uploadItem.setLastUploadDate(null);
+		uploadItem.setBatch(batchId);
+		fileIndex++;
+		//add to index
+		Index.addMine(newKey, board);
+		Index.add(newKey, board);
 
-                uploadItem.setState(this.nextState);
-                frame1.setGeneratingCHK(false);
-            }
-            else if( mode == MODE_GENERATE_CHK )
-            {
-                frame1.setGeneratingCHK(true);
-                logger.info("CHK generation started for file: " + file);
-                String chkkey = null;
-                
-                if( file.length() <= FcpInsert.smallestChunk )
-                {
-                    logger.info("File too short, doesn't need encoding.");
-                    // generate only CHK
-                    chkkey = FecTools.generateCHK(file);
-                }
-                else
-                {
-                    FecSplitfile splitfile = new FecSplitfile( file );
-                    boolean alreadyEncoded = splitfile.uploadInit();
-                    if( !alreadyEncoded )
-                    {
-                        try {
-                            splitfile.encode();
-                        }
-                        catch(Throwable t)
-                        {
-							logger.log(Level.SEVERE, "Encoding failed", t);
-                            uploadItem.setState(FrostUploadItemObject.STATE_IDLE);
-                            ((UploadTableModel)frame1.getInstance().getUploadTable().getModel()).updateRow(uploadItem);
-                            return;
-                        }
-                    }
-                    // yes, this destroys any upload progress, but we come only here if 
-                    // chkKey == null, so the file should'nt be uploaded until now 
-                    splitfile.createRedirectFile(false); // gen normal redirect file for CHK generation
-                    
-                    chkkey = FecTools.generateCHK(splitfile.getRedirectFile(), splitfile.getRedirectFile().length());
-                }
-                
-                if( chkkey != null )
-                {
-                    String prefix = new String("freenet:");
-                    if( chkkey.startsWith(prefix) ) chkkey = chkkey.substring(prefix.length());
-                }
-                else
-                {
-                    logger.warning("Could not generate CHK key for redirect file.");
-                }
-                uploadItem.setKey(chkkey);
-                
-                // test if the GetRequestsThread did set us the nextState field...
-                if( uploadItem.getNextState() > 0 )
-                {
-                    uploadItem.setState( uploadItem.getNextState() );
-                    uploadItem.setNextState(0); // reset nextState
-                }
-                else
-                {
-                    uploadItem.setState( this.nextState );
-                }
-                frame1.setGeneratingCHK(false);
-            }
-            ((UploadTableModel)frame1.getInstance().getUploadTable().getModel()).updateRow(uploadItem);
-        }
-        catch (Throwable t)
-        {
-			logger.log(Level.SEVERE, "Exception thrown in run()", t);
-        }
-        frame1.setGeneratingCHK(false);
-    }
+		uploadItem.setState(this.nextState);
+	}
+    
+	/**
+	 * 
+	 */
+	private void generateCHK() {
+		logger.info("CHK generation started for file: " + file);
+		String chkkey = null;
+
+		if (file.length() <= FcpInsert.smallestChunk) {
+			logger.info("File too short, doesn't need encoding.");
+			// generate only CHK
+			chkkey = FecTools.generateCHK(file);
+		} else {
+			FecSplitfile splitfile = new FecSplitfile(file);
+			boolean alreadyEncoded = splitfile.uploadInit();
+			if (!alreadyEncoded) {
+				try {
+					splitfile.encode();
+				} catch (Throwable t) {
+					logger.log(Level.SEVERE, "Encoding failed", t);
+					uploadItem.setState(FrostUploadItemObject.STATE_IDLE);
+					(
+						(UploadTableModel) frame1
+							.getInstance()
+							.getUploadTable()
+							.getModel())
+							.updateRow(
+						uploadItem);
+					return;
+				}
+			}
+			// yes, this destroys any upload progress, but we come only here if 
+			// chkKey == null, so the file should'nt be uploaded until now 
+			splitfile.createRedirectFile(false);
+			// gen normal redirect file for CHK generation
+
+			chkkey =
+				FecTools.generateCHK(
+					splitfile.getRedirectFile(),
+					splitfile.getRedirectFile().length());
+		}
+
+		if (chkkey != null) {
+			String prefix = new String("freenet:");
+			if (chkkey.startsWith(prefix))
+				chkkey = chkkey.substring(prefix.length());
+		} else {
+			logger.warning("Could not generate CHK key for redirect file.");
+		}
+		uploadItem.setKey(chkkey);
+
+		// test if the GetRequestsThread did set us the nextState field...
+		if (uploadItem.getNextState() > 0) {
+			uploadItem.setState(uploadItem.getNextState());
+			uploadItem.setNextState(0); // reset nextState
+		} else {
+			uploadItem.setState(this.nextState);
+		}
+	}
 
     /**Constructor*/
-    public UploadThread(FrostUploadItemObject ulItem, SettingsClass config, int mode, LocalIdentity newMyId)
+    public UploadThread(UploadTicker newTicker, FrostUploadItemObject ulItem, SettingsClass config, int mode, LocalIdentity newMyId)
     {
-        this(ulItem, config, mode, -1, newMyId);
+        this(newTicker, ulItem, config, mode, -1, newMyId);
     }
-    public UploadThread(FrostUploadItemObject ulItem, SettingsClass config, int mode, int nextState, LocalIdentity newMyId)
-    {
-        this.destination = ulItem.getFileName();
-        this.file = new File(ulItem.getFilePath());
-        
-        myId = newMyId;
+	public UploadThread(
+		UploadTicker newTicker,
+		FrostUploadItemObject ulItem,
+		SettingsClass config,
+		int newMode,
+		int newNextState,
+		LocalIdentity newMyId) {
+			
+		destination = ulItem.getFileName();
+		file = new File(ulItem.getFilePath());
 
-        this.uploadItem = ulItem;
+		myId = newMyId;
+		ticker = newTicker;
+		uploadItem = ulItem;
 
-        this.htl = config.getIntValue("htlUpload");
-        this.board = ulItem.getTargetBoard();
-        this.mode = mode; // true=upload file false=generate chk (do not upload)
-        this.nextState = nextState;
-        if( this.nextState < 0 )
-        {
-            this.nextState = FrostUploadItemObject.STATE_IDLE;
-        }
-    }
+		htl = config.getIntValue("htlUpload");
+		board = ulItem.getTargetBoard();
+		mode = newMode; // true=upload file false=generate chk (do not upload)
+		nextState = newNextState;
+		if (nextState < 0) {
+			nextState = FrostUploadItemObject.STATE_IDLE;
+		}
+	}
 }
