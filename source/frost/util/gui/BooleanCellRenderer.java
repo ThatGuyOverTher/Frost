aebc2215ff71b58109fffc945d7da423e644290f
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-05-16 10:15:29 +0000

    Added: a table cell renderer for boolean fields (actually a JCheckBox)

---
+++ source/frost/util/gui/BooleanCellRenderer.java
@@ -0,0 +1,52 @@
+/*
+ * Created on May 16, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package frost.util.gui;
+
+import java.awt.Component;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ * @author Administrator
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class BooleanCellRenderer extends JCheckBox implements TableCellRenderer {
+
+	/**
+	 * 
+	 */
+	public BooleanCellRenderer() {
+		super();
+		setHorizontalAlignment(JLabel.CENTER);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.table.TableCellRenderer#getTableCellRendererComponent(javax.swing.JTable, java.lang.Object, boolean, boolean, int, int)
+	 */
+	public Component getTableCellRendererComponent(
+		JTable table,
+		Object value,
+		boolean isSelected,
+		boolean hasFocus,
+		int row,
+		int column) {
+			
+		if (isSelected) {
+			setForeground(table.getSelectionForeground());
+			super.setBackground(table.getSelectionBackground());
+		} else {
+			setForeground(table.getForeground());
+			setBackground(table.getBackground());
+		}
+		setSelected((value != null && ((Boolean) value).booleanValue()));
+		return this;
+	}
+
+}
