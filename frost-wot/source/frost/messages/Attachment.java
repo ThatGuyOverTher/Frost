f3cca232eec2784407e14b9d9ca88fb1cf48305e
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-05-24 13:06:51 +0000

    Removed: getMessage() method: it was not being used and the classes that implemented this interface had a default implementation of that method (returning null) Added: Comparable interface. Code layout improved.

--- frost-wot/source/frost/messages/Attachment.java
+++ frost-wot/source/frost/messages/Attachment.java
@@ -19,39 +19,30 @@ import frost.XMLizable;
  * To change the template for this generated type comment go to
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
-public abstract class Attachment implements XMLizable {
-	public static final int FILE=0;
-	public static final int BOARD=1;
-	public static final int PERSON=2;
-	
+public abstract class Attachment implements XMLizable, Comparable {
+	public static final int FILE = 0;
+	public static final int BOARD = 1;
+	public static final int PERSON = 2;
+
 	private static Logger logger = Logger.getLogger(Attachment.class.getName());
-	
-	
+
 	/**
 	 * 
 	 * @return the type of this attachment
 	 */
 	public abstract int getType();
-	
-	/**
-	 * 
-	 * @return the message this attachment came from
-	 */
-	public abstract MessageObject getMessage();
-	
-	public static Attachment getInstance(Element e){
-		
-		assert e.getAttribute("type").length()>0 :
-			"attachment type not specified!";	
-		try{
-			if (e.getAttribute("type").equals("file")) 		
+
+	public static Attachment getInstance(Element e) {
+
+		assert e.getAttribute("type").length() > 0 : "attachment type not specified!";
+		try {
+			if (e.getAttribute("type").equals("file"))
 				return new FileAttachment(e);
 			else if (e.getAttribute("type").equals("board"))
 				return new BoardAttachment(e);
-			else 
+			else
 				return new PersonAttachment(e);
-		}
-		catch(SAXException ex){
+		} catch (SAXException ex) {
 			logger.log(Level.SEVERE, "Exception thrown in getInstance(Element e)", ex);
 			return null;
 		}
