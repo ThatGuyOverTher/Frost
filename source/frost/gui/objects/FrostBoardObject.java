f7e2b484b6cf64d3265693e97887ca1189a8fe59
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-05-25 08:09:17 +0000

    Code layout improved (with eclipse) Added: boardDescription attribute

--- source/frost/gui/objects/FrostBoardObject.java
+++ source/frost/gui/objects/FrostBoardObject.java
@@ -25,230 +25,70 @@ import javax.swing.tree.DefaultMutableTreeNode;
 
 import frost.*;
 
-public class FrostBoardObject extends DefaultMutableTreeNode implements Comparable
-{
-    private String boardName = null;
-    private String boardFileName = null;
-    private String b64FileName = null;
-    private long lastUpdateStartMillis = -1; // never updated
+public class FrostBoardObject extends DefaultMutableTreeNode implements Comparable {
 
-    private int newMessageCount = 0;
+	private boolean autoUpdateEnabled = true; // must apply, no default
+	private String b64FileName = null;
+	private String boardDescription;
+	private String boardFileName = null;
+	private String boardName = null;
+	private Boolean hideBad = null;
+	private Boolean hideCheck = null;
+	private Boolean hideNA = null;
 
-    private boolean isFolder = false;
+	// if isConfigured=true then below options may apply
+	private boolean isConfigured = false;
 
-    private boolean spammed = false;
-    private int numberBlocked = 0; // number of blocked messages for this board
+	private boolean isFolder = false;
 
-    private String publicKey = null;
-    private String privateKey = null;
+	private boolean isUpdating = false;
+	private long lastUpdateStartMillis = -1; // never updated
+	// following: if set to null then the default will be returned
+	private Integer maxMessageDisplay = null;
 
-    private boolean isUpdating = false;
+	private int newMessageCount = 0;
+	private int numberBlocked = 0; // number of blocked messages for this board
+	private String privateKey = null;
 
-    // if isConfigured=true then below options may apply
-    private boolean isConfigured = false;
+	private String publicKey = null;
+	private Boolean showSignedOnly = null;
 
-    private boolean autoUpdateEnabled = true; // must apply, no default
-    // following: if set to null then the default will be returned
-    private Integer maxMessageDisplay = null;
-    private Boolean showSignedOnly = null;
-    private Boolean hideBad = null;
-    private Boolean hideCheck = null;
-    private Boolean hideNA = null;
+	private boolean spammed = false;
 
-    /**
-     * Constructs a new FrostBoardObject wich is a Board.
-     */
-    public FrostBoardObject(String name)
-    {
-        super();
-        boardName = name;
-        boardFileName = Mixed.makeFilename( boardName.toLowerCase() );
-        
-        if (Mixed.containsForeign(name))
-        	b64FileName = Core.getCrypto().encode64(name);
-    }
-    /**
-     * Constructs a new FrostBoardObject wich is a Board.
-     */
-    public FrostBoardObject(String name, String pubKey, String privKey)
-    {
-        this(name);
-        setPublicKey( pubKey );
-        setPrivateKey( privKey );
-    }
-    /**
-     * Constructs a new FrostBoardObject.
-     * If isFold is true, this will be a folder, else a board.
-     */
-    public FrostBoardObject(String name, boolean isFold)
-    {
-        this(name);
-        isFolder = isFold;
-    }
+	/**
+	 * Constructs a new FrostBoardObject wich is a Board.
+	 */
+	public FrostBoardObject(String name) {
+		super();
+		boardName = name;
+		boardFileName = Mixed.makeFilename(boardName.toLowerCase());
 
-    public void setBoardName(String newname)
-    {
-        boardName = newname;
-		boardFileName = Mixed.makeFilename( boardName.toLowerCase() );
-		
-		if (Mixed.containsForeign(newname))
-					b64FileName = Core.getCrypto().encode64(newname);
-		else
-			b64FileName=null;
-		
-		File boardFile = new File(MainFrame.keypool+boardFileName);
-		if (!boardFile.exists() || !boardFile.isDirectory()) //if it doesn't exist already, strip foreign chars
-			boardFileName = Core.getCrypto().encode64(newname);
-        
-    }
+		if (Mixed.containsForeign(name))
+			b64FileName = Core.getCrypto().encode64(name);
+	}
+	/**
+	 * Constructs a new FrostBoardObject.
+	 * If isFold is true, this will be a folder, else a board.
+	 */
+	public FrostBoardObject(String name, boolean isFold) {
+		this(name);
+		isFolder = isFold;
+	}
+	/**
+	 * Constructs a new FrostBoardObject wich is a Board.
+	 */
+	public FrostBoardObject(String name, String pubKey, String privKey, String description) {
+		this(name);
+		setPublicKey(pubKey);
+		setPrivateKey(privKey);
+		boardDescription = description;
+	}
 
-    public boolean isWriteAccessBoard()
-    {
-        if( publicKey != null && privateKey != null )
-            return true;
-        return false;
-    }
-    public boolean isReadAccessBoard()
-    {
-        if( publicKey != null && privateKey == null )
-            return true;
-        return false;
-    }
-    public boolean isPublicBoard()
-    {
-        if( publicKey == null && privateKey == null )
-            return true;
-        return false;
-    }
-
-    public String getBoardName()
-    {
-        return boardName;
-    }
-    public String getBoardFilename()
-    {
-        return b64FileName == null ? boardFileName : b64FileName;
-    }
-
-    public String toString()
-    {
-        return boardName;
-    }
-
-    public boolean isFolder()
-    {
-        return isFolder;
-    }
-
-    public boolean isLeaf()
-    {
-        return (isFolder() == false);
-    }
-
-    public String getPublicKey()
-    {
-        return publicKey;
-    }
-    public void setPublicKey( String val )
-    {
-        if( val != null )
-            val = val.trim();
-        publicKey = val;
-    }
-    public String getPrivateKey()
-    {
-        return privateKey;
-    }
-    public void setPrivateKey( String val )
-    {
-        if( val != null )
-            val = val.trim();
-        privateKey = val;
-    }
-
-    public String getStateString()
-    {
-        // TODO: translate
-        if( isReadAccessBoard() )
-        {
-            return "read access";
-        }
-        else if( isWriteAccessBoard() )
-        {
-            return "write access";
-        }
-        else if( isPublicBoard() )
-        {
-            return "public board";
-        }
-        return "*ERROR*";
-    }
-
-
-//////////////////////////////////////////////
-// From BoardStats
-
-    public void incBlocked()
-    {
-        numberBlocked++;
-    }
-
-    public void resetBlocked()
-    {
-        numberBlocked=0;
-    }
-
-    public int getBlockedCount()
-    {
-        return numberBlocked;
-    }
-
-    public void setSpammed(boolean val)
-    {
-        spammed=val;
-    }
-
-    public boolean isSpammed()
-    {
-        return spammed;
-    }
-
-    public long getLastUpdateStartMillis()
-    {
-        return lastUpdateStartMillis;
-    }
-
-    public void setLastUpdateStartMillis(long millis)
-    {
-        lastUpdateStartMillis = millis;
-    }
-
-    public int getNewMessageCount()
-    {
-        return newMessageCount;
-    }
-    public void setNewMessageCount( int val )
-    {
-        newMessageCount = val;
-    }
-    public void incNewMessageCount()
-    {
-        newMessageCount++;
-    }
-    public void decNewMessageCount()
-    {
-        newMessageCount--;
-    }
-
-    public boolean containsNewMessage()
-    {
-        if( getNewMessageCount() > 0 )
-            return true;
-        return false;
-    }
-
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	public int compareTo(Object o) {
-		if (o instanceof FrostBoardObject) {
+		if (o instanceof FrostBoardObject) { 
 			FrostBoardObject board = (FrostBoardObject) o;
 			if (board.isFolder() == isFolder()) {
 				//If both objects are of the same kind, sort by name
@@ -262,36 +102,415 @@ public class FrostBoardObject extends DefaultMutableTreeNode implements Comparab
 		}
 	}
 
-    /**
-     * Returns the String that is shown in tree.
-     * Appends new message count in brackets behind board name.
-     */
-    public String getVisibleText()
-    {
-        if( getNewMessageCount() == 0 )
-        {
-            return toString();
-        }
+	/**
+	 * @return
+	 */
+	public boolean containsFolderNewMessages() {
+		FrostBoardObject board = this;
+		int childs = getChildCount(); 
+		boolean newMessage = false;
 
-        StringBuffer sb = new StringBuffer();
-        sb.append( toString() )
-          .append( " (")
-          .append( getNewMessageCount() )
-          .append(")");
+		for (int c = 0; c < childs; c++) {
+			FrostBoardObject childBoard = (FrostBoardObject) board.getChildAt(c);
+			if ((childBoard.isFolder() && childBoard.containsFolderNewMessages())
+				|| childBoard.containsNewMessage()) {
+				newMessage = true;
+				break;
+			}
+		}
+		return newMessage;
+	}
 
-        return sb.toString();
-    }
+	/**
+	 * @return
+	 */
+	public boolean containsNewMessage() {
+		if (getNewMessageCount() > 0)
+			return true; 
+		return false;
+	}
+	
+	/**
+	 * 
+	 */
+	public void decNewMessageCount() { 
+		newMessageCount--;
+	}
 
+	/**
+	 * @return
+	 */
+	public boolean getAutoUpdateEnabled() {
+		if (!isConfigured())
+			return true;
+		return autoUpdateEnabled;
+	}
 
-    public boolean isUpdating()
-    {
-        return isUpdating;
-    }
-    public void setUpdating( boolean val )
-    {
-        isUpdating = val;
-    }
-    
+	/**
+	 * @return
+	 */
+	public int getBlockedCount() {
+		return numberBlocked;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getBoardFilename() {
+		return b64FileName == null ? boardFileName : b64FileName;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getBoardName() {
+		return boardName;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean getHideBad() {
+		if (!isConfigured() || hideBad == null) {
+			// return default
+			return MainFrame.frostSettings.getBoolValue("hideBadMessages");
+		}
+		return hideBad.booleanValue();
+	}
+	
+	/**
+	 * @return
+	 */
+	public Boolean getHideBadObj() {
+		return hideBad;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean getHideCheck() {
+		if (!isConfigured() || hideCheck == null) {
+			// return default
+			return MainFrame.frostSettings.getBoolValue("hideCheckMessages");
+		}
+		return hideCheck.booleanValue();
+	}
+	
+	/**
+	 * @return
+	 */
+	public Boolean getHideCheckObj() {
+		return hideCheck;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean getHideNA() {
+		if (!isConfigured() || hideNA == null) {
+			// return default
+			return MainFrame.frostSettings.getBoolValue("hideNAMessages");
+		}
+		return hideNA.booleanValue();
+	}
+	
+	/**
+	 * @return
+	 */
+	public Boolean getHideNAObj() {
+		return hideNA;
+	}
+
+	/**
+	 * @return
+	 */
+	public long getLastUpdateStartMillis() {
+		return lastUpdateStartMillis;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getMaxMessageDisplay() {
+		if (!isConfigured() || maxMessageDisplay == null) {
+			// return default
+			return MainFrame.frostSettings.getIntValue("maxMessageDisplay");
+		}
+		return maxMessageDisplay.intValue();
+	}
+	/**
+	 * @return
+	 */
+	public Integer getMaxMessageDisplayObj() {
+		return maxMessageDisplay;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getNewMessageCount() {
+		return newMessageCount;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getPrivateKey() {
+		return privateKey;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getPublicKey() {
+		return publicKey;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean getShowSignedOnly() {
+		if (!isConfigured() || showSignedOnly == null) {
+			// return default
+			return MainFrame.frostSettings.getBoolValue("signedOnly");
+		}
+		return showSignedOnly.booleanValue();
+	}
+	/**
+	 * @return
+	 */
+	public Boolean getShowSignedOnlyObj() {
+		return showSignedOnly;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getStateString() {
+		// TODO: translate
+		if (isReadAccessBoard()) {
+			return "read access";
+		} else if (isWriteAccessBoard()) {
+			return "write access";
+		} else if (isPublicBoard()) {
+			return "public board";
+		}
+		return "*ERROR*";
+	}
+
+	/**
+	 * Returns the String that is shown in tree.
+	 * Appends new message count in brackets behind board name.
+	 */
+	public String getVisibleText() {
+		if (getNewMessageCount() == 0) {
+			return toString();
+		}
+
+		StringBuffer sb = new StringBuffer();
+		sb.append(toString()).append(" (").append(getNewMessageCount()).append(")");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 
+	 */
+	//////////////////////////////////////////////
+	// From BoardStats
+
+	public void incBlocked() {
+		numberBlocked++;
+	}
+	/**
+	 * 
+	 */
+	public void incNewMessageCount() {
+		newMessageCount++;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isConfigured() {
+		return isConfigured;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isFolder() {
+		return isFolder;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.tree.TreeNode#isLeaf()
+	 */
+	public boolean isLeaf() {
+		return (isFolder() == false);
+	}
+	
+	/**
+	 * @return
+	 */
+	public boolean isPublicBoard() {
+		if (publicKey == null && privateKey == null)
+			return true;
+		return false;
+	}
+	
+	/**
+	 * @return
+	 */
+	public boolean isReadAccessBoard() {
+		if (publicKey != null && privateKey == null)
+			return true;
+		return false;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isSpammed() {
+		return spammed;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isUpdating() {
+		return isUpdating;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isWriteAccessBoard() {
+		if (publicKey != null && privateKey != null)
+			return true;
+		return false;
+	}
+
+	/**
+	 * 
+	 */
+	public void resetBlocked() {
+		numberBlocked = 0;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setAutoUpdateEnabled(boolean val) {
+		autoUpdateEnabled = val;
+	}
+
+	/**
+	 * @param newname
+	 */
+	public void setBoardName(String newname) {
+		boardName = newname;
+		boardFileName = Mixed.makeFilename(boardName.toLowerCase());
+
+		if (Mixed.containsForeign(newname))
+			b64FileName = Core.getCrypto().encode64(newname);
+		else
+			b64FileName = null;
+
+		File boardFile = new File(MainFrame.keypool + boardFileName);
+		if (!boardFile.exists()
+			|| !boardFile.isDirectory()) //if it doesn't exist already, strip foreign chars
+			boardFileName = Core.getCrypto().encode64(newname);
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setConfigured(boolean val) {
+		isConfigured = val;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setHideBad(Boolean val) {
+		hideBad = val;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setHideCheck(Boolean val) {
+		hideCheck = val;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setHideNA(Boolean val) {
+		hideNA = val;
+	}
+
+	/**
+	 * @param millis
+	 */
+	public void setLastUpdateStartMillis(long millis) {
+		lastUpdateStartMillis = millis;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setMaxMessageDays(Integer val) {
+		maxMessageDisplay = val;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setNewMessageCount(int val) {
+		newMessageCount = val;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setPrivateKey(String val) {
+		if (val != null)
+			val = val.trim();
+		privateKey = val;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setPublicKey(String val) {
+		if (val != null)
+			val = val.trim();
+		publicKey = val;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setShowSignedOnly(Boolean val) {
+		showSignedOnly = val;
+	}
+
+	/**
+	 * @param val
+	 */
+	public void setSpammed(boolean val) {
+		spammed = val;
+	}
+	
+	/**
+	 * @param val
+	 */
+	public void setUpdating(boolean val) {
+		isUpdating = val;
+	}
+
 	/**
 	 * 
 	 */
@@ -299,134 +518,11 @@ public class FrostBoardObject extends DefaultMutableTreeNode implements Comparab
 		Collections.sort(children);
 	}
 
-
-    public boolean isConfigured()
-    {
-        return isConfigured;
-    }
-    public void setConfigured( boolean val )
-    {
-        isConfigured = val;
-    }
-
-    public boolean getAutoUpdateEnabled()
-    {
-        if( !isConfigured() )
-            return true;
-        return autoUpdateEnabled;
-    }
-    public void setAutoUpdateEnabled( boolean val )
-    {
-        autoUpdateEnabled = val;
-    }
-
-    public int getMaxMessageDisplay()
-    {
-        if( !isConfigured() || maxMessageDisplay == null )
-        {
-            // return default
-            return MainFrame.frostSettings.getIntValue("maxMessageDisplay");
-        }
-        return maxMessageDisplay.intValue();
-    }
-    public Integer getMaxMessageDisplayObj()
-    {
-        return maxMessageDisplay;
-    }
-    public void setMaxMessageDays( Integer val )
-    {
-        maxMessageDisplay = val;
-    }
-
-    public boolean getShowSignedOnly()
-    {
-        if( !isConfigured() || showSignedOnly == null )
-        {
-            // return default
-            return MainFrame.frostSettings.getBoolValue("signedOnly");
-        }
-        return showSignedOnly.booleanValue();
-    }
-    public Boolean getShowSignedOnlyObj()
-    {
-        return showSignedOnly;
-    }
-    public void setShowSignedOnly( Boolean val )
-    {
-        showSignedOnly = val;
-    }
-
-    public boolean getHideBad()
-    {
-        if( !isConfigured() || hideBad == null )
-        {
-            // return default
-            return MainFrame.frostSettings.getBoolValue("hideBadMessages");
-        }
-        return hideBad.booleanValue();
-    }
-    public Boolean getHideBadObj()
-    {
-        return hideBad;
-    }
-    public void setHideBad( Boolean val )
-    {
-        hideBad = val;
-    }
-
-    public boolean getHideCheck()
-    {
-        if( !isConfigured() || hideCheck == null )
-        {
-            // return default
-            return MainFrame.frostSettings.getBoolValue("hideCheckMessages");
-        }
-        return hideCheck.booleanValue();
-    }
-    public Boolean getHideCheckObj()
-    {
-        return hideCheck;
-    }
-    public void setHideCheck( Boolean val )
-    {
-        hideCheck = val;
-    }
-
-    public boolean getHideNA()
-    {
-        if( !isConfigured() || hideNA == null )
-        {
-            // return default
-            return MainFrame.frostSettings.getBoolValue("hideNAMessages");
-        }
-        return hideNA.booleanValue();
-    }
-    public Boolean getHideNAObj()
-    {
-        return hideNA;
-    }
-    public void setHideNA( Boolean val )
-    {
-        hideNA = val;
-    }
-
-    public boolean containsFolderNewMessages()
-    {
-        FrostBoardObject board = this;
-        int childs = getChildCount();
-        boolean newMessage = false;
-
-        for(int c=0; c<childs; c++)
-        {
-            FrostBoardObject childBoard = (FrostBoardObject)board.getChildAt(c);
-            if( (childBoard.isFolder() && childBoard.containsFolderNewMessages()) ||
-                childBoard.containsNewMessage() )
-            {
-                newMessage = true;
-                break;
-            }
-        }
-        return newMessage;
-    }
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return boardName;
+	}
 
 }
