30d1dc06003209991ecb3f7c2ef4958d06254d18
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-06-04 00:43:02 +0000

    Added: now the user can choose which columns the table should show (popup menu not implemented yet)

--- source/frost/util/model/gui/ModelTable.java
+++ source/frost/util/model/gui/ModelTable.java
@@ -8,18 +8,29 @@ package frost.util.model.gui;
 
 import java.awt.Font;
 import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 import java.util.logging.*;
 
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
+import javax.swing.event.TableColumnModelEvent;
+import javax.swing.table.*;
 
 import frost.util.model.*;
 
 /**
- * @author Administrator
- *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ * This subclass of AbstractTableModel is passed an OrderedModel and a
+ * TableFormat in the constructor. It then creates a JTable and displays 
+ * the content of the OrderedModel on it according to the rules specified
+ * in the TableFormat. 
+ * 
+ * It also listens for changes in the OrderedModel  and updates the JTable 
+ * as necessary.
+ * 
+ * Besides, the user can choose which columns will be shown via a menu
+ * that pops up when he right clicks on the header.
+ * 
+ * @author $Author$
+ * @version $Revision$
  */
 public class ModelTable extends AbstractTableModel {
 	/**
@@ -29,7 +40,7 @@ public class ModelTable extends AbstractTableModel {
 	private class Listener implements OrderedModelListener {
 	
 		/**
-		 * 
+		 * This constructor creates a new instance of Listener
 		 */
 		public Listener() {
 			super();
@@ -41,7 +52,8 @@ public class ModelTable extends AbstractTableModel {
 		public void itemChanged(int position, ModelItem item, int fieldID, Object oldValue, Object newValue) {
 			int[] columns = tableFormat.getColumnNumbers(fieldID);
 			for (int i = 0; i < columns.length; i++) {
-				fireTableCellUpdated(position, columns[i]);
+				int columnIndex = visibleColumns.indexOf(new Integer(columns[i]));
+				fireTableCellUpdated(position, columnIndex);
 			}
 		}
 
@@ -78,7 +90,7 @@ public class ModelTable extends AbstractTableModel {
 	/**
 	 * Helper class to be able to safely get the selection fron any thread
 	 */
-	private class SelectionGetter implements Runnable {
+	protected class SelectionGetter implements Runnable {
 
 		private final int MODE_SINGLE = 0;
 		private final int MODE_MULTIPLE = 1;
@@ -89,7 +101,9 @@ public class ModelTable extends AbstractTableModel {
 		ModelItem selectedItem;
 		
 		/**
-		 * 
+		 * This method returns an array of all the ModelItems that are 
+		 * selected in the JTable.
+		 *  @return an array containing the ModelItems that are selected 
 		 */
 		public ModelItem[] getSelectedItems() {
 			mode = MODE_MULTIPLE;
@@ -108,7 +122,12 @@ public class ModelTable extends AbstractTableModel {
 		}
 
 		/**
-		 * 
+		 * This method returns the ModelItem that is selected in 
+		 * the JTable (or the first one if there are several). If there is
+		 * none, it returns null.
+		 *  @return the ModelItem that is selected in the JTable, or
+		 * 			 the first one if there are several. null if there is
+		 * 			 none.
 		 */
 		public ModelItem getSelectedItem() {
 			mode = MODE_SINGLE;
@@ -126,7 +145,10 @@ public class ModelTable extends AbstractTableModel {
 			return selectedItem;
 		}
 
-		/* (non-Javadoc)
+		/** 
+		 * This method is executed in the Swing event thread. It gets the selected items
+		 * and places them in an attribute so that the methods getSelectedItem and getSelectedItems
+		 * can return them
 		 * @see java.lang.Runnable#run()
 		 */
 		public void run() {
@@ -161,9 +183,23 @@ public class ModelTable extends AbstractTableModel {
 	
 	protected JTable table;
 	private JScrollPane scrollPane;
+	
+	/**
+	 * This ArrayList contains the model indexes of the columns that are being shown
+	 */
+	protected ArrayList visibleColumns = new ArrayList();
+	
+	/**
+	 * This ArrayList contains all of the TableColumns that this ModelTable may show.
+	 */
+	private ArrayList columns = new ArrayList();
 
 	/**
-	 * 
+	 * This method creates an instance of Model table with the given ModelTableFormat
+	 * but without an OrderedModel. The method setModel should be called before 
+	 * initialization (this constructor does not perform that initialization).
+	 * @param newTableFormat the ModelTableFormat that defines the visual representation
+	 * 						  of the data in the OrderedModel.
 	 */
 	protected ModelTable(ModelTableFormat newTableFormat) {
 		super();
@@ -172,7 +208,11 @@ public class ModelTable extends AbstractTableModel {
 	}
 
 	/**
-	 * 
+	 * This method creates an instance of Model table with the given ModelTableFormat
+	 * and OrderedModel and initializes it.
+	 * @param newModel the OrderedModel that contains the data to be shown on the JTable 
+	 * @param newTableFormat the ModelTableFormat that defines the visual representation
+	 * 						  of the data in the OrderedModel.
 	 */
 	public ModelTable(OrderedModel newModel, ModelTableFormat newTableFormat) {
 		super();
@@ -184,14 +224,26 @@ public class ModelTable extends AbstractTableModel {
 	}
 	
 	/**
-	 * 
+	 * This method initializes the ModelTable. It creates the default TableColumns,
+	 * customizes the JTable and sets up the listener.
 	 */
 	protected void initialize() {
+		int columnCount = tableFormat.getColumnCount();
+		for (int i = 0; i < columnCount; i++) {
+			visibleColumns.add(new Integer(i));
+		}
+
 		table = new JTable(this);
 		scrollPane = new JScrollPane(table);
 		tableFormat.addTable(table);
+
 		tableFormat.customizeTable(this);
 		
+		TableColumnModel columnModel = table.getColumnModel();
+		for (int i = 0; i < columnModel.getColumnCount(); i++) {
+			columns.add(columnModel.getColumn(i));
+		}
+
 		model.addOrderedModelListener(listener);
 	}
 
@@ -199,7 +251,7 @@ public class ModelTable extends AbstractTableModel {
 	 * @see javax.swing.table.TableModel#getColumnCount()
 	 */
 	public int getColumnCount() {
-		return tableFormat.getColumnCount();
+		return visibleColumns.size();
 	}
 
 	/* (non-Javadoc)
@@ -213,12 +265,16 @@ public class ModelTable extends AbstractTableModel {
 	 * @see javax.swing.table.TableModel#getValueAt(int, int)
 	 */
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		return tableFormat.getCellValue(model.getItemAt(rowIndex), columnIndex);
+		Integer index = (Integer) visibleColumns.get(columnIndex);
+		return tableFormat.getCellValue(model.getItemAt(rowIndex), index.intValue());
 	}
 	
 	
 	/**
-	 * @param positions
+	 * This method is called whenever an event is received from the 
+	 * OrderedModel indicating that several items have been removed from it.
+	 * @param positions the positions of the ModelItems that have
+	 * been removed from the OrderedModel.
 	 */
 	protected void fireTableRowsDeleted(int[] positions) {
 		for (int i = 0; i < positions.length; i++) {
@@ -226,29 +282,36 @@ public class ModelTable extends AbstractTableModel {
 		}			
 	}
 	
-	/**
-	 * @return
+	/** 
+	 * This method returns an array of all the ModelItems that are 
+	 * selected in the JTable.
+	 * @return an array containing the ModelItems that are selected 
 	 */
 	public ModelItem[] getSelectedItems() {
 		return new SelectionGetter().getSelectedItems();
 	}
 	
 	/**
-	 * @return
+	 * This method returns the selected ModelItem, or the first one
+	 * if there was several of them. It returns null if there was none.
+	 * @return the selected ModelItem, or the first one if there was 
+	 * 			several of them. null if there was none.
 	 */
 	public ModelItem getSelectedItem() {
 		return new SelectionGetter().getSelectedItem();
 	}
 	
 	/**
-	 * @return
+	 * This method returns the number of rows that are selected.
+	 * @return the number of rows that are selected.
 	 */
 	public int getSelectedCount() {
 		return table.getSelectedRowCount();
 	}
 
 	/**
-	 * @return
+	 * This method returns the JScrollPane the JTable is created into.
+	 * @return the JScrollPane the JTable is created into.
 	 */
 	public JScrollPane getScrollPane() {
 		return scrollPane;
@@ -258,18 +321,23 @@ public class ModelTable extends AbstractTableModel {
 	 * @see javax.swing.table.TableModel#getColumnName(int)
 	 */
 	public String getColumnName(int column) {
-		return tableFormat.getColumnName(column);
+		Integer index = (Integer) visibleColumns.get(column);
+		return tableFormat.getColumnName(index.intValue());
 	}
 
 	/**
-	 * @return
+	 * This method returns the JTable that is used by this ModelTable 
+	 * to show the contents of its OrderedModel.
+	 * @return the JTable that is used by this ModelTable to show the
+	 * 			contents of its OrderedModel.
 	 */
 	public JTable getTable() {
 		return table;
 	}
 
 	/**
-	 * @param font
+	 * This method changes the Font of the JTable in this ModelTable
+	 * @param font the new font for the JTable in this ModelTable.
 	 */
 	public void setFont(Font font) {
 		table.setFont(font);
@@ -280,29 +348,86 @@ public class ModelTable extends AbstractTableModel {
 	 * @see javax.swing.table.TableModel#isCellEditable(int, int)
 	 */
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		return tableFormat.isColumnEditable(columnIndex);
+		Integer index = (Integer) visibleColumns.get(columnIndex);
+		return tableFormat.isColumnEditable(index.intValue());
+	}
+	
+	/**
+	 * This method is used to find out if the column with the given model index
+	 * is currently being shown or not.
+	 * @param columnIndex the model index of the column to find out if it is
+	 * 					   being shown or not.
+	 * @return true if the column is being shown. false otherwise.
+	 */
+	public boolean isColumnVisible(int columnIndex) {
+		int position = visibleColumns.indexOf(new Integer(columnIndex));
+		if (position != -1) {
+			return true;
+		} else {
+			return false;	
+		}
+	}
+	
+	/**
+	 * This method shows or hides a particular column. In case it tries to
+	 * show a column that is already being shown or to hide a column that is
+	 * already hidden, the command is simply ignored.
+	 * @param index the model index of the column to hide or show.
+	 * @param visible if true, the column will be shown. If false, the column
+	 * 		   will be hidden.
+	 */
+	public void setColumnVisible(int index, boolean visible) {
+		TableColumnModelEvent event =
+			new TableColumnModelEvent(getTable().getColumnModel(), index, index);
+		TableColumnModel columnModel = getTable().getColumnModel();
+			
+		if (visible) {
+			int position = visibleColumns.indexOf(new Integer(index));
+			if (position == -1) {
+				visibleColumns.add(new Integer(index));
+				TableColumn column = (TableColumn) columns.get(index);
+				column.setModelIndex(visibleColumns.size() - 1);
+				columnModel.addColumn(column);
+			}
+		} else {
+			int position = visibleColumns.indexOf(new Integer(index));
+			visibleColumns.remove(new Integer(index));
+			columnModel.removeColumn((TableColumn) columns.get(index));
+			for (int i = 0; i < columnModel.getColumnCount(); i++) {
+				TableColumn column = columnModel.getColumn(i);
+				int modelIndex = column.getModelIndex();
+				if (modelIndex >= position) {
+					column.setModelIndex(modelIndex - 1);
+				}	
+			}
+		}
 	}
 
 	/* (non-Javadoc)
 	 * @see javax.swing.table.TableModel#setValueAt(java.lang.Object, int, int)
 	 */
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-		tableFormat.setCellValue(aValue, model.getItemAt(rowIndex), columnIndex);
+		Integer index = (Integer) visibleColumns.get(columnIndex);
+		tableFormat.setCellValue(aValue, model.getItemAt(rowIndex), index.intValue());
 	}
 
 	/**
-	 * @param model
+	 * This method sets a new OrderedModel for the ModelTable (mainly to be
+	 * used in conjunction with the constructor that is only passed a ModelTableFormat)
+	 * @param model the OrderedModel this ModelTable will get the data from
 	 */
 	protected void setModel(OrderedModel newModel) {
 		model = newModel;
 	}
 
 	/**
-	 * This method returns the format of this model table
-	 * @return the format of this model table
+	 * This method returns an Iterator of all the TableColumns that this
+	 * ModelTable may show.
+	 * @return an Iterator of all the TableColumns that this
+	 * 			ModelTable may show.
 	 */
-	public ModelTableFormat getFormat() {
-		return tableFormat;
+	public Iterator getColumns() {
+		return columns.iterator();
 	}
 
 }
