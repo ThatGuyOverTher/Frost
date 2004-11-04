ca3b5d439f36c801cb749cf5813864613a140ea7
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-11-04 13:23:45 +0000

    Added: more incomplete classes of the new storage system

---
+++ frost-wot/source/frost/identities/IdentitiesXmlDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Created on Nov 4, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package frost.identities;
+
+/**
+ * @author $author$
+ * @version $revision$
+ */
+public class IdentitiesXmlDAO implements IdentitiesDAO {
+
+}
