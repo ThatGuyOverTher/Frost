f365c04f1ac152b0424548799fb392089edf51cb
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2004-12-01 01:47:34 +0000

    Refactoring: code layout improved.

--- frost-wot/source/frost/messages/MessageObject.java
+++ frost-wot/source/frost/messages/MessageObject.java
@@ -32,15 +32,310 @@ public class MessageObject implements XMLizable
 {
 	private static Logger logger = Logger.getLogger(MessageObject.class.getName());
 	
-	AttachmentList attachments; // this is never null!
+    private static final char[] evilChars = {'/', '\\', '*', '=', '|', '&', '#', '\"', '<', '>'}; // will be converted to _
+
+	private AttachmentList attachments; // this is never null!
+			//FIXME: this one is missing the "?" char as opposed to mixed.makeFilename
+    private String board, content, from, subject, date, time, index, publicKey, newContent;
+    
+    protected File file;
+
+    /**
+     * Constructor.
+     * Used to contruct an instance for a new message.
+     */
+    public MessageObject() {
+    this.board = "";
+    this.from = "";
+    this.subject = "";
+    this.board = "";
+    this.date = "";
+    this.time = "";
+    this.content = "";
+    this.publicKey = "";
+    this.attachments = new AttachmentList();
+    }
+
+    /**
+     * Constructor.
+     * Used to construct an instance for an existing messagefile.
+     */
+    public MessageObject(File file) throws Exception
+    {
+        this();
+        if( file == null || 
+            file.exists() == false ||
+            file.length() < 20 ) // prolog+needed tags are always > 20, but we need to filter 
+        {                        // out the messages containing "Empty" (encrypted for someone else)
+        	file.renameTo(new File("badMessage"));   
+            throw new Exception("Invalid input file for MessageObject, send the file \"badMessage\" to a dev");
+        }
+        this.file = file;
+        loadFile();
+        // ensure basic contents and formats
+        analyzeFile();
+    }
+
+    /**Set all values*/
+    public void analyzeFile() throws Exception
+    {
+        // set index for this msg from filename
+        String filename = file.getName();
+        this.index = (filename.substring(filename.lastIndexOf("-") + 1, filename.lastIndexOf(".xml"))).trim();
+        // ensure all needed fields are properly filled
+        if( from == null || date == null ||  time == null ||
+            board == null || !isValid() )
+        {
+        	String message = "Analyze file failed.  File saved as \"badMessage\", send to a dev.  Reason:\n";
+        	if (!isValid()) message = message + "isValid failed";
+        	if (content==null) message = message + "content null";
+        	logger.severe(message);
+        	file.renameTo(new File("badMessage"));
+            throw new Exception("Message have invalid or missing fields.");
+            
+        }
+        // replace evil chars
+        for( int i = 0; i < evilChars.length; i++ )
+        {
+            this.from = this.from.replace(evilChars[i], '_');
+            this.subject = this.subject.replace(evilChars[i], '_');
+            this.date = this.date.replace(evilChars[i], '_');
+            this.time = this.time.replace(evilChars[i], '_');
+        }
+    }
+    
+    /**
+     * @return
+     */
+    public AttachmentList getAttachmentList() {
+		return attachments;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getBoard() {
+		return board;
+	}
+
+	/**
+	 * Creates a Vector of Vectors which contains data for the attached boards
+	 * table.
+	 */
+	public Vector getBoardAttachments() {
+		Vector table = new Vector();
+		AttachmentList boards = attachments.getAllOfType(Attachment.BOARD);
+		Iterator i = boards.iterator();
+		while (i.hasNext()) {
+			BoardAttachment ba = (BoardAttachment) i.next();
+			FrostBoardObject aBoard = ba.getBoardObj();
+
+			Vector rows = new Vector();
+			rows.add(aBoard.getBoardName());
+			if (aBoard.getPublicKey() == null && aBoard.getPrivateKey() == null) {
+				rows.add("public");
+			} else if (aBoard.getPublicKey() != null && aBoard.getPrivateKey() == null) { 
+				rows.add("read - only");
+			} else {
+				rows.add("read / write");
+			}
+			if (aBoard.getDescription() == null) {
+				rows.add("Not present");
+			} else {
+				rows.add(aBoard.getDescription());
+			}
+			table.add(rows);
+		}
+		return table;
+	}
 	
+    /**
+     * @return
+     */
+    public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getDate() {
+		return date;
+	}
+
+	/**
+	 * @return
+	 */
+	public File getFile() {
+		return file;
+	}
+
+  /**
+   * Creates a Vector of Vectors which contains data for the attached files
+   * table.
+   */    
+    public Vector getFileAttachments() {
+        Vector table = new Vector();
+        AttachmentList files = attachments.getAllOfType(Attachment.FILE);
+        Iterator i = files.iterator();
+        while(i.hasNext())
+        {
+            FileAttachment fa = (FileAttachment)i.next();
+            SharedFileObject sfo = fa.getFileObj();
+            
+            if( sfo.getKey() != null && sfo.getKey().length() > 40 &&
+                sfo.getFilename() != null && sfo.getFilename().length() > 0 )
+            {
+                Vector rows = new Vector();
+                rows.add(sfo.getFilename());
+                rows.add(sfo.getSize());  //don't really need key
+                table.add(rows);
+            } 
+        }
+        return table;
+    }
+    /**
+     * @return
+     */
+    public String getFrom() {
+		return from;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getIndex() {
+		return index;
+	}
+    
+    /**
+     * @return
+     */
+    public List getOfflineFiles() {
+    	if (attachments == null) return null;
+    	
+    	List result = new LinkedList();
+    	
+    	List fileAttachments = attachments.getAllOfType(Attachment.FILE);
+    	Iterator it = fileAttachments.iterator();
+    	while (it.hasNext()) {
+    		SharedFileObject sfo = ((FileAttachment)it.next()).getFileObj();
+    		if (!sfo.isOnline())
+    			result.add(sfo);
+    	}
+    	return result;
+    }
+
+    /**
+     * @return
+     */
+    public String getPublicKey() {
+		return publicKey;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getSubject() {
+		return subject;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getTime() {
+		return time;
+	}
+	
+	/* (non-Javadoc)
+	 * @see frost.XMLizable#getXMLElement(org.w3c.dom.Document)
+	 */
 	public Element getXMLElement(Document d){
 		return messageObjectPopulateElement(d);
 	}
+
+    /**
+     * @return
+     */
+    public boolean isValid() {
+
+		if (subject == null)
+			subject = new String();
+		if (content == null)
+			content = new String();
+		if (date.equals(""))
+			return false;
+		if (time.equals(""))
+			return false;
+		// if (subject.equals(""))
+		//    return false;
+		if (board.equals(""))
+			return false;
+		if (from.equals(""))
+			return false;
+
+		if (from.length() > 256)
+			return false;
+		if (subject != null && subject.length() > 256)
+			return false;
+		if (board.length() > 256)
+			return false;
+		if (date.length() > 22)
+			return false;
+		if (content.length() > 32 * 1024)
+			return false;
+
+		return true;
+	}
+
+    /**
+	 * Parses the XML file and passes the FrostMessage element to XMLize load
+	 * method.
+	 */    
+    protected void loadFile() throws Exception
+    {
+        Document doc = null;
+        try {
+            doc = XMLTools.parseXmlFile(this.file, false);
+        } catch(Exception ex) {  // xml format error
+			File badMessage = new File("badmessage.xml");
+			if (file.renameTo(badMessage))
+				logger.log(Level.SEVERE, "Error - send the file badmessage.xml to a dev for analysis, more details below:", ex);
+        } 
+
+        if( doc == null )
+        {
+            throw new Exception("Error - MessageObject.loadFile: could'nt parse XML Document.");
+        }
+
+        Element rootNode = doc.getDocumentElement();
+
+        if( rootNode.getTagName().equals("FrostMessage") == false )
+        {
+        	File badMessage = new File("badmessage.xml");
+        	if (file.renameTo(badMessage))
+        		logger.severe("Error - send the file badmessage.xml to a dev for analysis.");
+            throw new Exception("Error - invalid message: does not contain the root tag 'FrostMessage'");
+        }
+        
+        // load the message load itself
+        loadXMLElement(rootNode);
+    }
 	
+	/* (non-Javadoc)
+	 * @see frost.XMLizable#loadXMLElement(org.w3c.dom.Element)
+	 */
+	public void loadXMLElement(Element e) throws SAXException {
+		messageObjectPopulateFromElement(e);
+	}
+	
+	/**
+	 * @param d
+	 * @return
+	 */
 	protected Element messageObjectPopulateElement(Document d){
 		Element el = d.createElement("FrostMessage");
-		
+	
 		CDATASection cdata;
 		Element current;
 		
@@ -97,10 +392,10 @@ public class MessageObject implements XMLizable
 		return el;
 	}
 	
-	public void loadXMLElement(Element e) throws SAXException {
-		messageObjectPopulateFromElement(e);
-	}
-	
+	/**
+	 * @param e
+	 * @throws SAXException
+	 */
 	protected void messageObjectPopulateFromElement(Element e)
 		throws SAXException {
 			
@@ -121,266 +416,59 @@ public class MessageObject implements XMLizable
             }
 	}
 
-    static final char[] evilChars = {'/', '\\', '*', '=', '|', '&', '#', '\"', '<', '>'}; // will be converted to _
-			//FIXME: this one is missing the "?" char as opposed to mixed.makeFilename
-    String board, content, from, subject, date, time, index, publicKey, newContent;
-    
-    File file;
-
-  /**
-   * Creates a Vector of Vectors which contains data for the
-   * attached files table.
-   */    
-    public Vector getFileAttachments() {
-        Vector table = new Vector();
-        AttachmentList files = attachments.getAllOfType(Attachment.FILE);
-        Iterator i = files.iterator();
-        while(i.hasNext())
-        {
-            FileAttachment fa = (FileAttachment)i.next();
-            SharedFileObject sfo = fa.getFileObj();
-            
-            if( sfo.getKey() != null && sfo.getKey().length() > 40 &&
-                sfo.getFilename() != null && sfo.getFilename().length() > 0 )
-            {
-                Vector rows = new Vector();
-                rows.add(sfo.getFilename());
-                rows.add(sfo.getSize());  //don't really need key
-                table.add(rows);
-            } 
-        }
-        return table;
-    }
-
 	/**
-	 * Creates a Vector of Vectors which contains data for the
-	 * attached boards table.
+	 * @param board
 	 */
-	public Vector getBoardAttachments() {
-		Vector table = new Vector();
-		AttachmentList boards = attachments.getAllOfType(Attachment.BOARD);
-		Iterator i = boards.iterator();
-		while (i.hasNext()) {
-			BoardAttachment ba = (BoardAttachment) i.next();
-			FrostBoardObject aBoard = ba.getBoardObj();
-
-			Vector rows = new Vector();
-			rows.add(aBoard.getBoardName());
-			if (aBoard.getPublicKey() == null && aBoard.getPrivateKey() == null) {
-				rows.add("public");
-			} else if (aBoard.getPublicKey() != null && aBoard.getPrivateKey() == null) { 
-				rows.add("read - only");
-			} else {
-				rows.add("read / write");
-			}
-			if (aBoard.getDescription() == null) {
-				rows.add("Not present");
-			} else {
-				rows.add(aBoard.getDescription());
-			}
-			table.add(rows);
-		}
-		return table;
+	public void setBoard(String board) {
+		this.board = board;
 	}
 
-    public String getPublicKey() {
-    return publicKey;
-    }
-    public void setPublicKey(String pk) {
-    publicKey=pk;
-    }
-    public String getBoard() {
-    return board;
-    }
-    public String getContent() {
-    return content;
-    }
-    public String getFrom() {
-    return from;
-    }
-    public String getSubject() {
-    return subject;
-    }
-    public String getDate() {
-    return date;
-    }
-    public String getTime() {
-    return time;
-    }
-    public String getIndex() {
-    return index;
-    }
-    public File getFile() {
-    return file;
-    }
+	/**
+	 * @param content
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-    /**Set*/
-    public void setBoard(String board) {
-    this.board = board;
-    }
-    public void setContent(String content) {
-    this.content = content;
-    }
-    public void setFrom(String from) {
-    this.from = from;
-    }
-    public void setSubject(String subject) {
-    this.subject = subject;
-    }
-    public void setDate(String date) {
-    this.date = date;
-    }
-    public void setTime(String time) {
-    this.time = time;
-    }
-    public void setIndex(String index) {
-    this.index = index;
-    }
+	/**
+	 * @param date
+	 */
+	public void setDate(String date) {
+		this.date = date;
+	}
 
-    public boolean isValid() {
+	/**
+	 * @param from
+	 */
+	public void setFrom(String from) {
+		this.from = from;
+	}
 
-	if (subject == null) subject = new String();
-	if (content == null) content = new String();
-    if (date.equals(""))
-        return false;
-    if (time.equals(""))
-        return false;
-   // if (subject.equals(""))
-    //    return false;
-    if (board.equals(""))
-        return false;
-    if (from.equals(""))
-        return false;
+	/**
+	 * @param index
+	 */
+	public void setIndex(String index) {
+		this.index = index;
+	}
 
-    if (from.length() > 256)
-        return false;
-    if (subject!=null && subject.length() > 256)
-        return false;
-    if (board.length() > 256)
-        return false;
-    if (date.length() > 22)
-        return false;
-    if (content.length() > 32*1024)
-        return false;
+	/**
+	 * @param pk
+	 */
+	public void setPublicKey(String pk) {
+		publicKey = pk;
+	}
 
-    return true;
-    }
+	/**
+	 * @param subject
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
 
-    /**Set all values*/
-    public void analyzeFile() throws Exception
-    {
-        // set index for this msg from filename
-        String filename = file.getName();
-        this.index = (filename.substring(filename.lastIndexOf("-") + 1, filename.lastIndexOf(".xml"))).trim();
-        // ensure all needed fields are properly filled
-        if( from == null || date == null ||  time == null ||
-            board == null || !isValid() )
-        {
-        	String message = "Analyze file failed.  File saved as \"badMessage\", send to a dev.  Reason:\n";
-        	if (!isValid()) message = message + "isValid failed";
-        	if (content==null) message = message + "content null";
-        	logger.severe(message);
-        	file.renameTo(new File("badMessage"));
-            throw new Exception("Message have invalid or missing fields.");
-            
-        }
-        // replace evil chars
-        for( int i = 0; i < evilChars.length; i++ )
-        {
-            this.from = this.from.replace(evilChars[i], '_');
-            this.subject = this.subject.replace(evilChars[i], '_');
-            this.date = this.date.replace(evilChars[i], '_');
-            this.time = this.time.replace(evilChars[i], '_');
-        }
-    }
-
-    /**
-     * Parses the XML file and passes the FrostMessage element to
-     * XMLize load method.
-     */    
-    protected void loadFile() throws Exception
-    {
-        Document doc = null;
-        try {
-            doc = XMLTools.parseXmlFile(this.file, false);
-        } catch(Exception ex) {  // xml format error
-			File badMessage = new File("badmessage.xml");
-			if (file.renameTo(badMessage))
-				logger.log(Level.SEVERE, "Error - send the file badmessage.xml to a dev for analysis, more details below:", ex);
-        } 
-
-        if( doc == null )
-        {
-            throw new Exception("Error - MessageObject.loadFile: could'nt parse XML Document.");
-        }
-
-        Element rootNode = doc.getDocumentElement();
-
-        if( rootNode.getTagName().equals("FrostMessage") == false )
-        {
-        	File badMessage = new File("badmessage.xml");
-        	if (file.renameTo(badMessage))
-        		logger.severe("Error - send the file badmessage.xml to a dev for analysis.");
-            throw new Exception("Error - invalid message: does not contain the root tag 'FrostMessage'");
-        }
-        
-        // load the message load itself
-        loadXMLElement(rootNode);
-    }
-
-    /**
-     * Constructor.
-     * Used to construct an instance for an existing messagefile.
-     */
-    public MessageObject(File file) throws Exception
-    {
-        this();
-        if( file == null || 
-            file.exists() == false ||
-            file.length() < 20 ) // prolog+needed tags are always > 20, but we need to filter 
-        {                        // out the messages containing "Empty" (encrypted for someone else)
-        	file.renameTo(new File("badMessage"));   
-            throw new Exception("Invalid input file for MessageObject, send the file \"badMessage\" to a dev");
-        }
-        this.file = file;
-        loadFile();
-        // ensure basic contents and formats
-        analyzeFile();
-    }
-
-    /**
-     * Constructor.
-     * Used to contruct an instance for a new message.
-     */
-    public MessageObject() {
-    this.board = "";
-    this.from = "";
-    this.subject = "";
-    this.board = "";
-    this.date = "";
-    this.time = "";
-    this.content = "";
-    this.publicKey = "";
-    this.attachments = new AttachmentList();
-    }
-    
-    public List getOfflineFiles() {
-    	if (attachments == null) return null;
-    	
-    	List result = new LinkedList();
-    	
-    	List fileAttachments = attachments.getAllOfType(Attachment.FILE);
-    	Iterator it = fileAttachments.iterator();
-    	while (it.hasNext()) {
-    		SharedFileObject sfo = ((FileAttachment)it.next()).getFileObj();
-    		if (!sfo.isOnline())
-    			result.add(sfo);
-    	}
-    	return result;
-    }
-    
-    public AttachmentList getAttachmentList()
-    {
-        return attachments;
-    }
+	/**
+	 * @param time
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
 }
