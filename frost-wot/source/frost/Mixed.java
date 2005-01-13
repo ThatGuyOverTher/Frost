6f0322a9f3550a32e7f9718914832789fbdbb4e8
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2005-01-13 14:45:10 +0000

    Refactoring: removed two methods that are not needed anymore.

--- frost-wot/source/frost/Mixed.java
+++ frost-wot/source/frost/Mixed.java
@@ -18,8 +18,6 @@
 */
 package frost;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.*;
 import java.io.*;
 import java.util.logging.Logger;
 
@@ -164,43 +162,6 @@ public static String makeSafeXML(String text) {
 
     	return false;
     }
-    /**
-     * If a string is on the system clipboard, this method returns it;
-     * otherwise it returns null.
-     *
-     * @return String  The String from system clipboard or null 
-     */
-    public static String getSystemClipboard()
-    {
-        Transferable t =
-            Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
-
-        try
-        {
-            if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor))
-            {
-                String text =
-                    (String)t.getTransferData(DataFlavor.stringFlavor);
-                return text;
-            }
-        }
-        catch (UnsupportedFlavorException e) {}
-        catch (IOException e) {}
-        
-        return null;
-    }
-
-    /**
-     * This method writes a string to the system clipboard.
-     *
-     * @param str  The String to be written to the system clipboard 
-     */
-    public static void setSystemClipboard(String str)
-    {
-        StringSelection ss = new StringSelection(str);
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
-    }
-    
     public static boolean binaryCompare(byte[] src, int offs, String searchTxt)
     {
         int searchLen = searchTxt.length();
