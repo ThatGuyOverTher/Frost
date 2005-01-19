fc12cc71e828f345cbd506f59c9f28ca926f627d
sleontux <sleontux@unknown>
2005-01-19 18:46:24 +0000

    *** empty log message ***

--- source/frost/gui/MessageWindow.java
+++ source/frost/gui/MessageWindow.java
@@ -5,11 +5,16 @@
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
 package frost.gui;
+
 import frost.messages.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import java.beans.*;
+import java.util.logging.Logger;
+
+import frost.SettingsClass;
 import frost.util.gui.*;
 
 /**
@@ -18,12 +23,16 @@ import frost.util.gui.*;
  */
 public class MessageWindow extends JFrame{
 	private final MessageObject message;
-	private AntialiasedTextArea textfield;
+	private AntialiasedTextArea messageTextArea;
 	private JScrollPane scrollpane;
 	private MessageWindow messageWindow;
 	private Listener listener;
+	private SettingsClass settings;
+	private Logger logger = Logger.getLogger(MessageWindow.class.getName());
+	private Window parentWindow;
 	
-	private class Listener extends KeyAdapter implements KeyListener{
+	
+	private class Listener extends KeyAdapter implements KeyListener, PropertyChangeListener{
 		
 		public void keyPressed(KeyEvent e){
 			maybeDoSomething(e);
@@ -35,12 +44,27 @@ public class MessageWindow extends JFrame{
 			}
 		}
 		
+		public void propertyChange( PropertyChangeEvent evt){
+			
+			if (evt.getPropertyName().equals(SettingsClass.MESSAGE_BODY_FONT_NAME)) {
+				fontChanged();
+			}
+			if (evt.getPropertyName().equals(SettingsClass.MESSAGE_BODY_FONT_SIZE)) {
+				fontChanged();
+			}
+			if (evt.getPropertyName().equals(SettingsClass.MESSAGE_BODY_FONT_STYLE)) {
+				fontChanged();
+			}
+		}
+		
 	}
 	
-	public MessageWindow(MessageObject message, Dimension size){
+	public MessageWindow(SettingsClass frostSettings, Window parentWindow, MessageObject message, Dimension size){
 		super();
 		this.setSize(size);
 		this.message = message;
+		this.parentWindow = parentWindow;
+		settings = frostSettings;
 		initialize();
 	}
 	
@@ -50,18 +74,63 @@ public class MessageWindow extends JFrame{
 		messageWindow = this;
 		this.setTitle(message.getSubject());
 		
-		textfield = new AntialiasedTextArea();
-		textfield.setWrapStyleWord(true);
-		textfield.setLineWrap(true);
-		textfield.setEditable(false);
-		textfield.setText(message.getContent());
-		textfield.addKeyListener(listener);
+		messageTextArea = new AntialiasedTextArea();
+		messageTextArea.setWrapStyleWord(true);
+		messageTextArea.setLineWrap(true);
+		messageTextArea.setEditable(false);
+		messageTextArea.setText(message.getContent());
+		messageTextArea.addKeyListener(listener);
 		this.addKeyListener(listener);
 		
+		settings.addPropertyChangeListener(SettingsClass.MESSAGE_BODY_FONT_NAME, listener);
+		settings.addPropertyChangeListener(SettingsClass.MESSAGE_BODY_FONT_SIZE, listener);
+		settings.addPropertyChangeListener(
+			SettingsClass.MESSAGE_BODY_FONT_STYLE,
+			listener);
+		settings.addPropertyChangeListener("messageBodyAA", listener);
+		
+		fontChanged();
+		
 		scrollpane = new JScrollPane();
-		scrollpane.getViewport().add(textfield);
+		scrollpane.getViewport().add(messageTextArea);
 		
 		this.getContentPane().setLayout(new BorderLayout());
 		this.getContentPane().add(scrollpane, BorderLayout.CENTER );
+		
+		ImageIcon frameIcon = new ImageIcon(MessageWindow.class.getResource("/data/messagebright.gif"));
+		this.setIconImage(frameIcon.getImage());
+		this.setLocationRelativeTo(parentWindow);
+		
 	}
+	
+	/**
+	 * 
+	 */
+	private void fontChanged() {
+		String fontName = settings.getValue(SettingsClass.MESSAGE_BODY_FONT_NAME);
+		int fontStyle = settings.getIntValue(SettingsClass.MESSAGE_BODY_FONT_STYLE);
+		int fontSize = settings.getIntValue(SettingsClass.MESSAGE_BODY_FONT_SIZE);
+		Font font = new Font(fontName, fontStyle, fontSize);
+		if (!font.getFamily().equals(fontName)) {
+			logger.severe(
+				"The selected font was not found in your system\n"
+					+ "That selection will be changed to \"Monospaced\".");
+			settings.setValue(SettingsClass.MESSAGE_BODY_FONT_NAME, "Monospaced");
+			font = new Font("Monospaced", fontStyle, fontSize);
+		}
+		messageTextArea.setFont(font);
+
+		fontName = settings.getValue(SettingsClass.MESSAGE_LIST_FONT_NAME);
+		fontStyle = settings.getIntValue(SettingsClass.MESSAGE_LIST_FONT_STYLE);
+		fontSize = settings.getIntValue(SettingsClass.MESSAGE_LIST_FONT_SIZE);
+		font = new Font(fontName, fontStyle, fontSize);
+		if (!font.getFamily().equals(fontName)) {
+			logger.severe(
+				"The selected font was not found in your system\n"
+					+ "That selection will be changed to \"SansSerif\".");
+			settings.setValue(SettingsClass.MESSAGE_LIST_FONT_NAME, "SansSerif");
+			font = new Font("SansSerif", fontStyle, fontSize);
+		}
+	}
+
 }
