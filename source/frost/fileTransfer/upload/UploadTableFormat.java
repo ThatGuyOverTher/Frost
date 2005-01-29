95417f97cc526ff8aaa8ea78e2cba7852ee096e0
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2005-01-29 23:59:32 +0000

    Added: GUI for enabling / disabling upload items.

--- source/frost/fileTransfer/upload/UploadTableFormat.java
+++ source/frost/fileTransfer/upload/UploadTableFormat.java
@@ -12,6 +12,7 @@ import java.util.Comparator;
 import javax.swing.*;
 import javax.swing.table.*;
 
+import frost.util.gui.BooleanCell;
 import frost.util.gui.translation.*;
 import frost.util.model.ModelItem;
 import frost.util.model.gui.*;
@@ -147,6 +148,21 @@ class UploadTableFormat extends SortedTableFormat implements LanguageListener {
 	}
 	
 	/**
+	 * This inner class implements the comparator for the column "Enabled"
+	 */
+	private class EnabledComparator implements Comparator {
+
+		/* (non-Javadoc)
+		 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+		 */
+		public int compare(Object o1, Object o2) {
+			FrostUploadItem item1 = (FrostUploadItem) o1;
+			FrostUploadItem item2 = (FrostUploadItem) o2;
+			return item1.isEnabled().equals(item2.isEnabled()) ? 0 : 1 ;
+		}
+	}
+	
+	/**
 	 * This inner class implements the comparator for the column "Key"
 	 */
 	private class KeyComparator implements Comparator {
@@ -184,7 +200,7 @@ class UploadTableFormat extends SortedTableFormat implements LanguageListener {
 
 	private Language language;
 
-	private final static int COLUMN_COUNT = 6;
+	private final static int COLUMN_COUNT = 7;
 	
 	private String stateUploadedNever;
 	private String stateRequested;
@@ -204,24 +220,26 @@ class UploadTableFormat extends SortedTableFormat implements LanguageListener {
 		language.addLanguageListener(this);
 		refreshLanguage();
 		
-		setComparator(new NameComparator(), 0);
-		setComparator(new FileSizeComparator(), 1);
-		setComparator(new StateComparator(), 2);
-		setComparator(new PathComparator(), 3);
-		setComparator(new DestinationComparator(), 4);
-		setComparator(new KeyComparator(), 5);
+		setComparator(new EnabledComparator(), 0);
+		setComparator(new NameComparator(), 1);
+		setComparator(new FileSizeComparator(), 2);
+		setComparator(new StateComparator(), 3);
+		setComparator(new PathComparator(), 4);
+		setComparator(new DestinationComparator(), 5);
+		setComparator(new KeyComparator(), 6);
 	}
 
 	/**
 	 * 
 	 */
 	private void refreshLanguage() {
-		setColumnName(0, language.getString("Filename"));
-		setColumnName(1, language.getString("Size"));
-		setColumnName(2, language.getString("Last upload"));
-		setColumnName(3, language.getString("Path"));
-		setColumnName(4, language.getString("Destination"));
-		setColumnName(5, language.getString("Key"));
+		setColumnName(0, language.getString("DownloadTableFormat.Enabled"));
+		setColumnName(1, language.getString("Filename"));
+		setColumnName(2, language.getString("Size"));
+		setColumnName(3, language.getString("Last upload"));
+		setColumnName(4, language.getString("Path"));
+		setColumnName(5, language.getString("Destination"));
+		setColumnName(6, language.getString("Key"));
 		
 		stateUploadedNever = language.getString("Never");
 		stateRequested = language.getString("Requested");
@@ -234,27 +252,48 @@ class UploadTableFormat extends SortedTableFormat implements LanguageListener {
 	}
 	
 	/* (non-Javadoc)
+	 * @see frost.util.model.gui.ModelTableFormat#setCellValue(java.lang.Object, frost.util.model.ModelItem, int)
+	 */
+	public void setCellValue(Object value, ModelItem item, int columnIndex) {
+		FrostUploadItem uploadItem = (FrostUploadItem) item;
+		switch (columnIndex) {
+
+			case 0 : //Enabled
+				Boolean valueBoolean = (Boolean) value;
+				uploadItem.setEnabled(valueBoolean);
+				break;
+
+			default :
+				super.setCellValue(value, item, columnIndex);
+		}
+	}
+	
+	/* (non-Javadoc)
 	 * @see frost.util.model.gui.ModelTableFormat#getCellValue(frost.util.model.ModelItem, int)
 	 */
 	public Object getCellValue(ModelItem item, int columnIndex) {
 		FrostUploadItem uploadItem = (FrostUploadItem) item;
 		switch (columnIndex) {
-			case 0 :	//Filename
+		
+			case 0 : //Enabled
+				return uploadItem.isEnabled();
+		
+			case 1 :	//Filename
 				return uploadItem.getFileName();
 								
-			case 1 :	//Size
+			case 2 :	//Size
 				return uploadItem.getFileSize();
 				
-			case 2 :	//Last upload
+			case 3 :	//Last upload
 				return getStateAsString(uploadItem, uploadItem.getState());
 				
-			case 3 :	//Path
+			case 4 :	//Path
 				return uploadItem.getFilePath();
 				
-			case 4 :	//Destination
+			case 5 :	//Destination
 				return uploadItem.getTargetBoard().getName();
 				
-			case 5 :	//Key
+			case 6 :	//Key
 				if (uploadItem.getKey() == null) {
 					return unknown;
 				} else {
@@ -320,16 +359,21 @@ class UploadTableFormat extends SortedTableFormat implements LanguageListener {
 		
 		//Sets the relative widths of the columns
 		TableColumnModel columnModel = modelTable.getTable().getColumnModel();
-		int[] widths = { 250, 80, 80, 80, 80 };
+		int[] widths = { 30, 250, 80, 80, 80, 80 };
 		for (int i = 0; i < widths.length; i++) {
 			columnModel.getColumn(i).setPreferredWidth(widths[i]);
 		}
 		
+		// Column "Enabled"
+		columnModel.getColumn(0).setCellRenderer(BooleanCell.RENDERER);
+		columnModel.getColumn(0).setCellEditor(BooleanCell.EDITOR);
+		setColumnEditable(0, true);
+		
 		// Column "Name"
-		columnModel.getColumn(0).setCellRenderer(new NameRenderer((SortedModelTable) modelTable));
+		columnModel.getColumn(1).setCellRenderer(new NameRenderer((SortedModelTable) modelTable));
 		
 		// Column "Size"
-		columnModel.getColumn(1).setCellRenderer(new FileSizeRenderer());		
+		columnModel.getColumn(2).setCellRenderer(new FileSizeRenderer());		
 	}
 
 	/* (non-Javadoc)
@@ -338,34 +382,37 @@ class UploadTableFormat extends SortedTableFormat implements LanguageListener {
 	public int[] getColumnNumbers(int fieldID) {
 		switch (fieldID) {
 			case FrostUploadItem.FIELD_ID_DONE_BLOCKS :
-				return new int[] {2};	//Last upload
+				return new int[] {3};	//Last upload
 				
 			case FrostUploadItem.FIELD_ID_FILE_NAME :
-				return new int[] {0};	//Filename
+				return new int[] {1};	//Filename
 				
 			case FrostUploadItem.FIELD_ID_FILE_PATH :
-				return new int[] {3};	//Path
+				return new int[] {4};	//Path
 			
 			case FrostUploadItem.FIELD_ID_FILE_SIZE :
-				return new int[] {1};	//Size
+				return new int[] {2};	//Size
 			
 			case FrostUploadItem.FIELD_ID_KEY :
-				return new int[] {5};	//Key
+				return new int[] {6};	//Key
 				
 			case FrostUploadItem.FIELD_ID_LAST_UPLOAD_DATE :
-				return new int[] {2};	//Last upload
+				return new int[] {3};	//Last upload
 			
 			case FrostUploadItem.FIELD_ID_SHA1 :
-				return new int[] {0};	//Filename
+				return new int[] {1};	//Filename
 					
 			case FrostUploadItem.FIELD_ID_STATE :
-				return new int[] {2};	//Last upload
+				return new int[] {3};	//Last upload
 				
 			case FrostUploadItem.FIELD_ID_TARGET_BOARD :
-				return new int[] {4};	//Destination
+				return new int[] {5};	//Destination
 				
 			case FrostUploadItem.FIELD_ID_TOTAL_BLOCKS :
-				return new int[] {2};	//Last upload
+				return new int[] {3};	//Last upload
+			
+			case FrostUploadItem.FIELD_ID_ENABLED :
+				return new int[] {0};	//Enabled
 				
 			default :	
 				return new int[] {};
