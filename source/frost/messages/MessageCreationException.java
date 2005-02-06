39e20869e033ac1895e0fd59084147e2922db467
José Manuel Arnesto <kevloral@soF1qyGm2zHO+aN+Wu9sTeYSSFY>
2005-02-06 00:44:58 +0000

    Refactoring: new type of exception added to more appropriately describe the failure in the creation of a MessageObject

---
+++ source/frost/messages/MessageCreationException.java
@@ -0,0 +1,56 @@
+/*
+ * Created on 06-feb-2005
+ * 
+ */
+package frost.messages;
+
+/**
+ * @author $Author$
+ * @version $Revision$
+ */
+public class MessageCreationException extends Exception {
+
+	private boolean empty;
+	
+	/**
+	 * This method creates a new MessageCreationException with the given
+	 * message.
+	 * @param message a string describing the exception
+	 */
+	public MessageCreationException(String message) {
+		super(message);
+	}
+	
+	/**
+	 * This method creates a new MessageCreationException with the given
+	 * message and empty property.
+	 * @param message a string describing the exception
+	 * @param empty true if message creation failed because the file only contained
+	 * 		  the word "Empty". False if the reason was different.
+	 */
+	public MessageCreationException(String message, boolean empty) {
+		super(message);
+		this.empty = empty;
+	}
+	
+	/**
+	 * This method returns true if message creation failed because the file 
+	 * only contained the word "Empty"
+	 * @return true if message creation failed because the file only contained
+	 * 				the word "Empty". False if the reason was different.
+	 */
+	public boolean isEmpty() {
+		return empty;
+	}
+	
+	/**
+	 * This method sets the empty property of the exception, used to distinguish
+	 * between the file only containing the word "Empty" and other reasons for
+	 * the failure.
+	 * @param empty true if message creation failed because the file only contained
+	 * 				the word "Empty". False if the reason was different.
+	 */
+	public void setEmpty(boolean empty) {
+		this.empty = empty;
+	}
+}
