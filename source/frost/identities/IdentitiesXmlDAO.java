fe912465217c68c176e5d0c0dde21b3548ce6b78
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2005-09-08 06:21:00 +0000

    myId is not longer saved in friends list, removed NA key handling (unneeded)

--- source/frost/identities/IdentitiesXmlDAO.java
+++ source/frost/identities/IdentitiesXmlDAO.java
@@ -1,9 +1,21 @@
 /*
- * Created on Nov 4, 2004
- *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
+  IdentitiesXmlDAO.java / Frost
+  Copyright (C) 2001  Jan-Thomas Czornack <jantho@users.sourceforge.net>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 package frost.identities;
 
 import java.io.*;
@@ -243,6 +255,10 @@ public class IdentitiesXmlDAO implements IdentitiesDAO {
 		Element rootElement = d.createElement("FrostIdentities");
 		//first save myself
 		rootElement.appendChild(identities.getMyId().getXMLElement(d));
+        
+        // myId is in friends list too, dont save it there
+        identities.getFriends().remove(identities.getMyId().getUniqueName());
+        
 		//then friends
 		Element friends = identities.getFriends().getXMLElement(d);
 		friends.setAttribute("type", "friends");
@@ -255,6 +271,10 @@ public class IdentitiesXmlDAO implements IdentitiesDAO {
 		Element neutral = identities.getNeutrals().getXMLElement(d);
 		neutral.setAttribute("type", "neutral");
 		rootElement.appendChild(neutral);
+        
+        // finally put myId back to friends
+        identities.getFriends().add(identities.getMyId());
+        
 		d.appendChild(rootElement);
 
 		//We save identities to "identities.xml.tmp"
