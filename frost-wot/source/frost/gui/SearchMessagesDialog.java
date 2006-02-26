d0cac5fe0f61111a038c5324eb823b578ff988e6
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2006-02-26 19:21:18 +0000

    preliminary search dialog, GUI elements mainly finished

--- frost-wot/source/frost/gui/SearchMessagesDialog.java
+++ frost-wot/source/frost/gui/SearchMessagesDialog.java
@@ -1,4 +1,21 @@
-package frost.gui;
+/*
+  SearchMessagesDialog.java / Frost
+  Copyright (C) 2006  Jan-Thomas Czornack <jantho@users.sourceforge.net>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/package frost.gui;
 
 import java.awt.*;
 
@@ -6,7 +23,7 @@ import javax.swing.*;
 
 import frost.gui.model.*;
 
-public class SearchMessageDialog extends JDialog {
+public class SearchMessagesDialog extends JDialog {
 
     private JPanel jContentPane = null;
     private JPanel contentPanel = null;
@@ -62,13 +79,13 @@ public class SearchMessageDialog extends JDialog {
     /**
      * This is the default constructor
      */
-    public SearchMessageDialog() {
+    public SearchMessagesDialog() {
         super();
         initialize();
     }
 
     public static void main(String[] args) {
-        new SearchMessageDialog().setVisible(true);
+        new SearchMessagesDialog().setVisible(true);
     }
     
     /**
