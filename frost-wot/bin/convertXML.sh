61724052dbd701e2882e73bcd8a4dafe7fc15770
saces <saces@freenetproject.org>
2006-03-27 08:22:57 +0000

    Changed backslash to slash

--- frost-wot/bin/convertXML.sh
+++ frost-wot/bin/convertXML.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp .;frost.jar;lib\xercesImpl.jar,lib\xml-apis.jar frost.ext.ConvertXMLUTF16ToUTF8
+java -cp .;frost.jar;lib/xercesImpl.jar,lib/xml-apis.jar frost.ext.ConvertXMLUTF16ToUTF8
