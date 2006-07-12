8c4704fa106532fb0ea6b61d4faebd8f222b742d
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2006-07-12 08:44:43 +0000

    for encrypted msgs, show receiver identity

--- frost-wot/source/frost/gui/MessageWindow.java
+++ frost-wot/source/frost/gui/MessageWindow.java
@@ -55,8 +55,6 @@ public class MessageWindow extends JFrame {
         this.setSize(size);
         this.searchMessagesConfig = smc;
 
-//      FIXME: for encrypted msgs show a receiver line below from line!!! maybe in messagepanel too?
-
         initialize();
 
         // set visible BEFORE updating the textpane to allow correct positioning of dividers
@@ -101,33 +99,16 @@ public class MessageWindow extends JFrame {
     }
 
     private class Listener extends WindowAdapter implements KeyListener, WindowListener {
-        /* (non-Javadoc)
-         * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
-         */
         public void keyPressed(KeyEvent e) {
             maybeDoSomething(e);
         }
-        /* (non-Javadoc)
-         * @see java.awt.event.KeyListener#keyReleased(java.awt.event.KeyEvent)
-         */
         public void keyReleased(KeyEvent e) {
-            //Nothing
         }
-        /* (non-Javadoc)
-         * @see java.awt.event.KeyListener#keyTyped(java.awt.event.KeyEvent)
-         */
         public void keyTyped(KeyEvent e) {
-            //Nothing
         }
-        /* (non-Javadoc)
-         * @see java.awt.event.WindowListener#windowClosing(java.awt.event.WindowEvent)
-         */
         public void windowClosing(WindowEvent e) {
             close();
         }
-        /**
-         * @param e
-         */
         public void maybeDoSomething(KeyEvent e){
             if( e.getKeyChar() == KeyEvent.VK_ESCAPE ) {
                 close();
@@ -139,9 +120,11 @@ public class MessageWindow extends JFrame {
 
         private JLabel Lsubject = null;
         private JLabel Lfrom = null;
+        private JLabel Lto = null;
         private JLabel Ldate = null;
         private JTextField TFsubject = null;
         private JTextField TFfrom = null;
+        private JTextField TFto = null;
         private JTextField TFdate = null;
         private JLabel Lboard = null;
         private JTextField TFboard = null;
@@ -152,6 +135,9 @@ public class MessageWindow extends JFrame {
         public MessageWindowTopPanel(FrostMessageObject msg) {
             super();
             innerMessage = msg;
+
+//          FIXME: for encrypted msgs show a receiver line below from line!!! maybe in messagepanel too?
+
             initialize();
             languageChanged(null);
             language.addLanguageListener(this);
@@ -177,87 +163,120 @@ public class MessageWindow extends JFrame {
             language.removeLanguageListener(this);
         }
 
+        // subject, from, (to), date/board
         private void initialize() {
-            GridBagConstraints gridBagConstraints41 = new GridBagConstraints();
-            gridBagConstraints41.gridx = 5;
-            gridBagConstraints41.anchor = java.awt.GridBagConstraints.NORTHEAST;
-            gridBagConstraints41.gridheight = 3;
-            gridBagConstraints41.insets = new java.awt.Insets(5,5,5,5);
-            gridBagConstraints41.gridy = 1;
-            GridBagConstraints gridBagConstraints11 = new GridBagConstraints();
-            gridBagConstraints11.fill = java.awt.GridBagConstraints.NONE;
-            gridBagConstraints11.gridy = 3;
-            gridBagConstraints11.weightx = 0.0;
-            gridBagConstraints11.anchor = java.awt.GridBagConstraints.WEST;
-            gridBagConstraints11.insets = new java.awt.Insets(1,1,1,5);
-            gridBagConstraints11.gridx = 3;
-            GridBagConstraints gridBagConstraints6 = new GridBagConstraints();
-            gridBagConstraints6.gridx = 2;
-            gridBagConstraints6.insets = new java.awt.Insets(1,8,1,2);
-            gridBagConstraints6.anchor = java.awt.GridBagConstraints.WEST;
-            gridBagConstraints6.gridy = 3;
             Lboard = new JLabel();
             Lboard.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 12));
-            GridBagConstraints gridBagConstraints5 = new GridBagConstraints();
-            gridBagConstraints5.fill = java.awt.GridBagConstraints.NONE;
-            gridBagConstraints5.gridy = 3;
-            gridBagConstraints5.weightx = 0.0;
-            gridBagConstraints5.insets = new java.awt.Insets(1,1,1,5);
-            gridBagConstraints5.anchor = java.awt.GridBagConstraints.WEST;
-            gridBagConstraints5.gridx = 1;
-            GridBagConstraints gridBagConstraints4 = new GridBagConstraints();
-            gridBagConstraints4.fill = java.awt.GridBagConstraints.NONE;
-            gridBagConstraints4.gridy = 2;
-            gridBagConstraints4.weightx = 1.0;
-            gridBagConstraints4.gridwidth = 4;
-            gridBagConstraints4.insets = new java.awt.Insets(1,1,1,5);
-            gridBagConstraints4.anchor = java.awt.GridBagConstraints.WEST;
-            gridBagConstraints4.gridx = 1;
-            GridBagConstraints gridBagConstraints3 = new GridBagConstraints();
-            gridBagConstraints3.fill = java.awt.GridBagConstraints.HORIZONTAL;
-            gridBagConstraints3.gridy = 1;
-            gridBagConstraints3.weightx = 1.0;
-            gridBagConstraints3.gridwidth = 4;
-            gridBagConstraints3.insets = new java.awt.Insets(1,1,1,5);
-            gridBagConstraints3.anchor = java.awt.GridBagConstraints.WEST;
-            gridBagConstraints3.gridx = 1;
-            GridBagConstraints gridBagConstraints2 = new GridBagConstraints();
-            gridBagConstraints2.gridx = 0;
-            gridBagConstraints2.insets = new java.awt.Insets(1,5,1,2);
-            gridBagConstraints2.anchor = java.awt.GridBagConstraints.WEST;
-            gridBagConstraints2.gridy = 3;
             Ldate = new JLabel();
             Ldate.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 12));
-            GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
-            gridBagConstraints1.gridx = 0;
-            gridBagConstraints1.insets = new java.awt.Insets(1,5,1,2);
-            gridBagConstraints1.anchor = java.awt.GridBagConstraints.WEST;
-            gridBagConstraints1.gridy = 2;
             Lfrom = new JLabel();
             Lfrom.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 12));
-            GridBagConstraints gridBagConstraints = new GridBagConstraints();
-            gridBagConstraints.gridx = 0;
-            gridBagConstraints.insets = new java.awt.Insets(1,5,1,2);
-            gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-            gridBagConstraints.gridy = 1;
             Lsubject = new JLabel();
             Lsubject.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 12));
+            Lto = new JLabel();
+            Lto.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 12));
+
+            GridBagConstraints BreplyConstraints = new GridBagConstraints(); // Breply
+            BreplyConstraints.gridx = 5;
+            BreplyConstraints.anchor = java.awt.GridBagConstraints.NORTHEAST;
+            BreplyConstraints.gridheight = 3;
+            BreplyConstraints.insets = new java.awt.Insets(5,5,5,5);
+            BreplyConstraints.gridy = 1;
+
+            GridBagConstraints LsubjectConstraints = new GridBagConstraints();  // Lsubject
+            LsubjectConstraints.gridx = 0;
+            LsubjectConstraints.insets = new java.awt.Insets(1,5,1,2);
+            LsubjectConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            LsubjectConstraints.gridy = 1;
+
+            GridBagConstraints TFsubjectConstraints = new GridBagConstraints(); // TFsubject
+            TFsubjectConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+            TFsubjectConstraints.gridy = 1;
+            TFsubjectConstraints.weightx = 1.0;
+            TFsubjectConstraints.gridwidth = 4;
+            TFsubjectConstraints.insets = new java.awt.Insets(1,1,1,5);
+            TFsubjectConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            TFsubjectConstraints.gridx = 1;
+
+            GridBagConstraints LfromConstraints = new GridBagConstraints(); // Lfrom
+            LfromConstraints.gridx = 0;
+            LfromConstraints.insets = new java.awt.Insets(1,5,1,2);
+            LfromConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            LfromConstraints.gridy = 2;
+
+            GridBagConstraints TFfromConstraints = new GridBagConstraints(); // TFfrom
+            TFfromConstraints.fill = java.awt.GridBagConstraints.NONE;
+            TFfromConstraints.gridy = 2;
+            TFfromConstraints.weightx = 1.0;
+            TFfromConstraints.gridwidth = 4;
+            TFfromConstraints.insets = new java.awt.Insets(1,1,1,5);
+            TFfromConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            TFfromConstraints.gridx = 1;
+
+            GridBagConstraints LtoConstraints = new GridBagConstraints(); // Lto
+            LtoConstraints.gridx = 0;
+            LtoConstraints.insets = new java.awt.Insets(1,5,1,2);
+            LtoConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            LtoConstraints.gridy = 3;
+
+            GridBagConstraints TFtoConstraints = new GridBagConstraints(); // TFto
+            TFtoConstraints.fill = java.awt.GridBagConstraints.NONE;
+            TFtoConstraints.gridy = 3;
+            TFtoConstraints.weightx = 1.0;
+            TFtoConstraints.gridwidth = 4;
+            TFtoConstraints.insets = new java.awt.Insets(1,1,1,5);
+            TFtoConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            TFtoConstraints.gridx = 1;
+
+            GridBagConstraints LdateConstraints = new GridBagConstraints(); // Ldate
+            LdateConstraints.gridx = 0;
+            LdateConstraints.insets = new java.awt.Insets(1,5,1,2);
+            LdateConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            LdateConstraints.gridy = 4;
+
+            GridBagConstraints TFdateConstraints = new GridBagConstraints(); // TFdate
+            TFdateConstraints.fill = java.awt.GridBagConstraints.NONE;
+            TFdateConstraints.gridy = 4;
+            TFdateConstraints.weightx = 0.0;
+            TFdateConstraints.insets = new java.awt.Insets(1,1,1,5);
+            TFdateConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            TFdateConstraints.gridx = 1;
+            
+            GridBagConstraints LboardConstraints = new GridBagConstraints(); // Lboard
+            LboardConstraints.gridx = 2;
+            LboardConstraints.insets = new java.awt.Insets(1,8,1,2);
+            LboardConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            LboardConstraints.gridy = 4;
+
+            GridBagConstraints TFboardConstraints = new GridBagConstraints(); // TFboard
+            TFboardConstraints.fill = java.awt.GridBagConstraints.NONE;
+            TFboardConstraints.gridy = 4;
+            TFboardConstraints.weightx = 0.0;
+            TFboardConstraints.anchor = java.awt.GridBagConstraints.WEST;
+            TFboardConstraints.insets = new java.awt.Insets(1,1,1,5);
+            TFboardConstraints.gridx = 4;
+            
             this.setLayout(new GridBagLayout());
             this.setSize(new java.awt.Dimension(496,254));
-            this.add(Lsubject, gridBagConstraints);
-            this.add(Lfrom, gridBagConstraints1);
-            this.add(Ldate, gridBagConstraints2);
-            this.add(getTFsubject(), gridBagConstraints3);
-            this.add(getTFfrom(), gridBagConstraints4);
-            this.add(getTFdate(), gridBagConstraints5);
-            this.add(Lboard, gridBagConstraints6);
-            this.add(getTFboard(), gridBagConstraints11);
-            this.add(getBreply(), gridBagConstraints41);
+            this.add(Lsubject, LsubjectConstraints);
+            this.add(Lfrom, LfromConstraints);
+            this.add(Ldate, LdateConstraints);
+            this.add(getTFsubject(), TFsubjectConstraints);
+            this.add(getTFfrom(), TFfromConstraints);
+            if( innerMessage.getRecipientName() != null ) {
+                this.add(Lto, LtoConstraints);
+                this.add(getTFto(), TFtoConstraints);
+            }
+            this.add(getTFdate(), TFdateConstraints);
+            this.add(Lboard, LboardConstraints);
+            this.add(getTFboard(), TFboardConstraints);
+            this.add(getBreply(), BreplyConstraints);
         }
 
         public void languageChanged(LanguageEvent event) {
             Lsubject.setText(language.getString("MessageWindow.subject")+":");
             Lfrom.setText(language.getString("MessageWindow.from")+":");
+            Lto.setText(language.getString("MessageWindow.to")+":");
             Ldate.setText(language.getString("MessageWindow.date")+":");
             Lboard.setText(language.getString("MessageWindow.board")+":");
         }
@@ -280,6 +299,16 @@ public class MessageWindow extends JFrame {
                 TFfrom.setEditable(false);
             }
             return TFfrom;
+        }
+
+        private JTextField getTFto() {
+            if( TFto == null ) {
+                TFto = new JTextField();
+                TFto.setText(" "+innerMessage.getRecipientName());
+                TFto.setBorder(javax.swing.BorderFactory.createEmptyBorder(2,2,2,2));
+                TFto.setEditable(false);
+            }
+            return TFto;
         }
 
         private JTextField getTFdate() {
