659c8d05596931de81c9482258da21d919e0acde
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2006-09-02 12:06:21 +0000

    updates

--- source/frost/fcp/fcpjob/IFcpJob.java
+++ source/frost/fcp/fcpjob/IFcpJob.java
@@ -1,3 +1,21 @@
+/*
+ IFcpJob.java / Frost
+ Copyright (C) 2006  Frost Project <jtcfrost.sourceforge.net>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2 of
+ the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 package frost.fcp.fcpjob;
 
 /**
@@ -5,6 +23,6 @@ package frost.fcp.fcpjob;
  * and processes its job. 
  * It provides status informations and a priority.
  */
-public class IFcpJob {
+public interface IFcpJob {
 
 }
