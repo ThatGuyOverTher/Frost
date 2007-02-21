a606505418124859a6fe1d07f6f80d51f3025cee
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2007-02-21 12:30:16 +0000

    delete key deletes message, also when selection is in preview pane

--- source/frost/gui/MessageTextPane.java
+++ source/frost/gui/MessageTextPane.java
@@ -324,9 +324,10 @@ public class MessageTextPane extends JPanel {
             public void keyTyped(KeyEvent e) {
                 if( e == null ) {
                     return;
-                }
-                if(e.getKeyChar() == 'n' && parentFrame == mainFrame ) {
+                } else if(e.getKeyChar() == KeyEvent.VK_N && parentFrame == mainFrame ) {
                     mainFrame.getMessagePanel().selectNextUnreadMessage();
+                } else if(e.getKeyChar() == KeyEvent.VK_DELETE && parentFrame == mainFrame ) {
+                    mainFrame.getMessagePanel().deleteSelectedMessage();
                 }
             }
         });
