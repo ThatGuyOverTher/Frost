be9bed24b1a4b3efec584123ef66fc49b5da3080
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2009-03-08 11:16:57 +0000

    - lowered default for file owner cleanup again - removed language keys for values that changed their meaning in english

--- source/frost/gui/preferences/ExpirationPanel.java
+++ source/frost/gui/preferences/ExpirationPanel.java
@@ -268,7 +268,7 @@ class ExpirationPanel extends JPanel {
         CbKeepUnread.setText(language.getString("Options.expiration.keepUnreadMessages"));
 
         CbRemoveOfflineFilesWithKey.setText(language.getString("Options.expiration.removeOfflineFilesWithKey"));
-        LofflineFilesMaxDaysOld.setText(language.getString("Options.expiration.offlineFilesMaxDaysOld")+" (90)");
+        LofflineFilesMaxDaysOld.setText(language.getString("Options.expiration.offlineFilesMaxDaysOld")+" (30)");
 
         LcleanupIntervalDays.setText(language.getString("Options.expiration.cleanupIntervalDays") +
                 " (5 "+language.getString("Options.common.days")+")");
