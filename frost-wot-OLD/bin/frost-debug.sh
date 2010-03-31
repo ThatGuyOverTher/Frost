0d7d789e6a0b4c99daa460bd6f08aad3497959b6
Karsten Graul <bback@xgVRApPk+Yngy+jmtOeGzIbN_A0>
2010-03-31 06:39:13 +0000

    Remove old trunk (already tagged)

--- frost-wot-OLD/bin/frost-debug.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Xmx96M -ea -jar frost.jar "$@"
--- frost-wot-OLD/miscSources/ant_frostrevision/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem adapt for your OS and environment ;)
-set CLASSPATH=.;C:\Tools\ant\lib\ant.jar
-c:\tools\jdk15\bin\javac frost/buildsupport/FrostRevision.java
-jar cf frostsvn.jar frost/buildsupport/*.class
-pause
