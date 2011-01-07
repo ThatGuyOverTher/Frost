91a577c462238651f6d4f7a09b748d8f35ff45d2
Jan Gerritsen <artur@K7dLGJvoXF_QQeUhZq9bNp0lFx4>
2011-01-07 22:31:23 +0000

    Updated credits in about box.

--- frost-wot/source/frost/gui/AboutBox.java
+++ frost-wot/source/frost/gui/AboutBox.java
@@ -36,7 +36,7 @@ public class AboutBox extends JDialogWithDetails {
     // because a growing amount of users use CVS version:
     private String version = null;
 
-    private final static String copyright = "Copyright 2009 Frost Project";
+    private final static String copyright = "Copyright 2011 Frost Project";
     private final static String comments2 = "http://jtcfrost.sourceforge.net/";
 
     private final JPanel imagePanel = new JPanel();
@@ -67,6 +67,7 @@ public class AboutBox extends JDialogWithDetails {
         // Image panel
         imagePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         imagePanel.add(imageLabel);
+        
 
         // Messages panel
         final GridLayout gridLayout = new GridLayout(5, 1);
@@ -82,7 +83,7 @@ public class AboutBox extends JDialogWithDetails {
         messagesPanel.add(copyrightLabel);
         messagesPanel.add(licenseLabel);
         messagesPanel.add(websiteLabel);
-
+        
         // Putting everything together
         getUserPanel().setLayout(new BorderLayout());
         getUserPanel().add(imagePanel, BorderLayout.WEST);
@@ -93,28 +94,21 @@ public class AboutBox extends JDialogWithDetails {
 
     private void fillDetailsArea() {
         final StringBuilder details = new StringBuilder();
-        details.append(language.getString("AboutBox.text.development") + "\n");
-        details.append("   Jan Gerritsen\n");
-        details.append("   Karsten Graul (suspended)\n");
+        details.append(language.getString("AboutBox.text.development") + "\n\n");
+        details.append(language.getString("AboutBox.text.active") + "\n");
+        details.append("   Jan Gerritsen \n   (artur@K7dLGJvoXF_QQeUhZq9bNp0lFx4)\n\n");
+        details.append("   Karsten Graul \n   (bback@xgVRApPk+Yngy+jmtOeGzIbN_A0)\n\n");
+        details.append(language.getString("AboutBox.text.left") + "\n");
         details.append("   S. Amoako (quit)\n");
         details.append("   Roman Glebov (quit)\n");
         details.append("   Jan-Thomas Czornack (quit)\n");
         details.append("   Thomas Mueller (quit)\n");
         details.append("   Jim Hunziker (quit)\n");
         details.append("   Stefan Majewski (quit)\n");
-        details.append("   Edward Louis Severson IV (lazy)\n");
-        details.append("   José Manuel Arnesto (quit)\n\n");
-//        details.append(language.getString("AboutBox.text.windowsInstaller") + "\n");
-//        details.append("   Benoit Laniel\n\n");
-        details.append(language.getString("AboutBox.text.systemTrayExecutables") + "\n");
-        details.append("   Ingo Franzki\n\n");
-//        details.append(language.getString("AboutBox.text.translationSupport") + "\n");
-//        details.append("   Rudolf Krist\n");
-//        details.append("   RapHHfr\n\n");
-        details.append(language.getString("AboutBox.text.splashScreenLogo") + "\n");
-        details.append("   Frédéric Scheer\n\n");
-//        details.append(language.getString("AboutBox.text.miscCodeContributions") + "\n");
-//        details.append("   SuperSlut Yoda");
+        details.append("   Edward Louis Severson IV (quit)\n");
+        details.append("   José Manuel Arnesto (quit)\n");
+        details.append("   Ingo Franzki (old systray icon code)\n");
+        details.append("   Frédéric Scheer (splashscreen logo)\n");
         setDetailsText(details.toString());
     }
 
